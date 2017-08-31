12e867c775a67cd887338b5432110d05c39df247
Duncan Nixon <duncannixon@me.com>
2017-08-31 11:45:09 +0100

    Refactored helper.

--- src/views/helpers/getFormGroupClass.js
+++ src/views/helpers/getFormGroupClass.js
@@ -1,13 +1,12 @@
 // getFormGroupClass Helper
 // Usage: `{{#getFormGroupClass}}`
-// Inspects the input validation error (if there is one) and returns the correct combination of
-// form group classes to be used
+// Inspects the input validation error (if there is one) and returns the correct form group class to be used
 module.exports = (options, context) => {
   if (options === undefined) {
     // There is no validation error
-    return 'form-group'
+    return ''
   } else {
     // There is a validation error
-    return 'form-group form-group-error'
+    return 'form-group-error'
   }
 }
