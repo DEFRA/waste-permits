2a71c44ddf113f49dc8098bf769fb96237aea2f7
Duncan Nixon <duncannixon@me.com>
2017-09-04 17:34:49 +0100

    Optimised solution odata query.

--- src/models/dynamicsSolution.model.js
+++ src/models/dynamicsSolution.model.js
@@ -3,31 +3,29 @@
 const DynamicsDalService = require('../services/dynamicsDal.service')
 const BaseModel = require('./base.model')
 
-const DYNAMICS_COMPONENT_NAMES = [
-  'Core',
-  'Licensing and Permitting',
-  'Waste Permits'
-]
-
 module.exports = class DynamicsSolution extends BaseModel {
   static async get (authToken) {
     const dynamicsDal = new DynamicsDalService(authToken)
-    const query = encodeURI('solutions?$select=friendlyname,installedon,uniquename,version&$filter=isvisible eq true')
+
+    // The three solutions we are interested in are:
+    // - Core
+    // - Licensing and Permitting
+    // - Waste Permits
+    const filter = `isvisible eq true and
+      friendlyname eq 'Core' or
+      friendlyname eq 'Licensing and Permitting' or
+      friendlyname eq 'Waste Permits'`
+
+    const query = encodeURI(`solutions?$select=friendlyname,version&$filter=${filter}`)
     try {
       const response = await dynamicsDal.search(query)
 
-      // The three solutions we are interested in are:
-      // - Core
-      // - Licensing and Permitting
-      // - Waste Permits
       const dynamicsVersionInfo = []
       for (let solution of response) {
-        if (DYNAMICS_COMPONENT_NAMES.includes(solution.friendlyname)) {
-          dynamicsVersionInfo.push({
-            componentName: solution.friendlyname,
-            version: solution.version
-          })
-        }
+        dynamicsVersionInfo.push({
+          componentName: solution.friendlyname,
+          version: solution.version
+        })
       }
       return dynamicsVersionInfo
     } catch (error) {
