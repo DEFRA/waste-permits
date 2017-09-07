611556f398348442652caae888f2b80dcb32a512
Duncan Nixon <duncannixon@me.com>
2017-09-07 11:50:31 +0100

    Defensive coding - improved validator error handling when the validation messages have not been provided.

--- src/validators/base.validator.js
+++ src/validators/base.validator.js
@@ -10,17 +10,30 @@ module.exports = class BaseValidator {
     pageContext.errorList = []
 
     validationErrors.data.details.forEach((error) => {
+      console.log(error)
+
       const fieldName = error.path
-      pageContext.errors[fieldName] = this.errorMessages[fieldName][error.type]
 
-      if (!pageContext.errors[fieldName]) {
-        pageContext.errors[fieldName] = `Validation message not found... Field: [${fieldName}] Error Type: [${error.type}]`
+      if (!this.errorMessages[fieldName]) {
+        // Handle validation messages missing in the validator
+        pageContext.errors[fieldName] = `Unable to find error messages for field: ${fieldName}`
+        pageContext.errorList.push({
+          fieldName: fieldName,
+          message: pageContext.errors[fieldName]
+        })
+      } else {
+        // Look up the corresponding error message for the field that is in error and add to the page context
+        pageContext.errors[fieldName] = this.errorMessages[fieldName][error.type]
+
+        if (!pageContext.errors[fieldName]) {
+          pageContext.errors[fieldName] = `Validation message not found... Field: [${fieldName}] Error Type: [${error.type}]`
+        }
+
+        pageContext.errorList.push({
+          fieldName: fieldName,
+          message: pageContext.errors[fieldName]
+        })
       }
-
-      pageContext.errorList.push({
-        fieldName: error.path,
-        message: pageContext.errors[error.path]
-      })
     })
   }
 }
