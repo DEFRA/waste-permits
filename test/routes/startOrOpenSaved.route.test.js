6bfccfcd2e89f2740aa4778eb96633373edc50e1
Duncan Nixon <duncannixon@me.com>
2017-09-12 14:13:55 +0100

    Fixed merge problem.

--- test/routes/startOrOpenSaved.route.test.js
+++ test/routes/startOrOpenSaved.route.test.js
@@ -7,25 +7,15 @@ const server = require('../../index')
 
 const DOMParser = require('xmldom').DOMParser
 
-<<<<<<< HEAD
 let validateCookieStub
-=======
-let validateTokenStub
->>>>>>> c32ba3f3703d2dafc6d1d6336c96dfec9fc08e02
 
 let routePath = '/start/start-or-open-saved'
 
 lab.beforeEach((done) => {
   // Stub methods
-<<<<<<< HEAD
   validateCookieStub = server.methods.validateCookie
   server.methods.validateCookie = (cookie) => {
     return true
-=======
-  validateTokenStub = server.methods.validateToken
-  server.methods.validateToken = () => {
-    return 'my_token'
->>>>>>> c32ba3f3703d2dafc6d1d6336c96dfec9fc08e02
   }
 
   done()
@@ -33,11 +23,7 @@ lab.beforeEach((done) => {
 
 lab.afterEach((done) => {
   // Restore stubbed methods
-<<<<<<< HEAD
   server.methods.validateCookie = validateCookieStub
-=======
-  server.methods.validateToken = validateTokenStub
->>>>>>> c32ba3f3703d2dafc6d1d6336c96dfec9fc08e02
 
   done()
 })
@@ -90,28 +76,6 @@ lab.experiment('Start or Open Saved page tests:', () => {
     })
   })
 
-<<<<<<< HEAD
-=======
-  lab.test('POST Start or Open Saved page redirects to error screen when the user token is invalid', (done) => {
-    const request = {
-      method: 'POST',
-      url: routePath,
-      headers: {},
-      payload: {}
-    }
-
-    server.methods.validateToken = () => {
-      return undefined
-    }
-
-    server.inject(request, (res) => {
-      Code.expect(res.statusCode).to.equal(302)
-      Code.expect(res.headers['location']).to.equal('/error')
-      done()
-    })
-  })
-
->>>>>>> c32ba3f3703d2dafc6d1d6336c96dfec9fc08e02
   lab.test('POST Start or Open Saved page shows the error message summary panel when new or open has not been selected', (done) => {
     const request = {
       method: 'POST',
