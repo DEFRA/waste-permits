ec7714454223abb3cff4a49f7dffde4d794ac271
Duncan Nixon <duncannixon@me.com>
2017-09-13 13:37:36 +0100

    Added debugging to try work out why Travis is failing.

--- src/services/serverLogging.service.js
+++ src/services/serverLogging.service.js
@@ -4,6 +4,7 @@ const Constants = require('../constants')
 
 module.exports = class ServerLoggingService {
   static logError (message) {
+    console.error('ERROR: ', message)
     const server = require('../../server')
     server.log(Constants.LogLevel.ERROR, message)
   }
