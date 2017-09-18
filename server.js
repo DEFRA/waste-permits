281f83c76fe5cb90526e5933cd4ceb4aa1531a47
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2017-09-18 09:34:28 +0100

    Sign session cookie with password
    In order to improve security this change amends the config we provide Hapi to create the session cookie, to tell it the cookie must be [signed](https://hapijs.com/api#serverstatename-options).
    
    Specifically we will provide Hapi with an object that contains a password that will be used to calculate an [HMAC](https://en.wikipedia.org/wiki/Hash-based_message_authentication_code) for cookie integrity validation. This does not provide privacy, but a means to verify that the cookie value was generated by the server.

--- server.js
+++ server.js
@@ -45,6 +45,9 @@ server.state(Constants.COOKIE_KEY, {
   isHttpOnly: true,         // and non-secure
   isSameSite: 'Strict',     // Don't attach cookies on cross-site requests, preventing CSRF attacks
   encoding: 'base64json',   // Base 64 JSON encoded
+  sign: {                   // Sign values assigned to the cookie to ensure they came from the server
+    password: config.cookieValidationPassword
+  },
   clearInvalid: false,      // Remove invalid cookies
   strictHeader: true        // Don't allow violations of RFC 6265
 })
