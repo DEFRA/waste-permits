400db67e0143bded45e63c70c27f4636818f918f
Duncan Nixon <duncannixon@me.com>
2017-10-06 11:15:51 +0100

    Ensured that the final task list item (Check before sending) is always available. Refactored sections out into defineStandardRuleSections method. Refactored section numbers and names out into the Constants file. Unit testing.

--- src/models/standardRule.model.js
+++ src/models/standardRule.model.js
@@ -15,141 +15,127 @@ module.exports = class StandardRule extends BaseModel {
       this.code = dataObject.code
       this.codeForId = StandardRule.transformPermitCode(dataObject.code)
     }
+  }
 
-    // Create the Standard Rule permit sections
+  defineStandardRuleSections () {
     this.sections = []
 
     const beforeYouApplySection = {
-      sectionIndex: 1,
-      sectionName: 'Before you apply',
+      id: 'before-you-apply-section',
+      sectionNumber: 1,
+      sectionName: Constants.TaskList.SectionHeadings.BEFORE_YOU_APPLY,
       sectionItems: [{
         id: 'check-permit-cost-and-time',
         label: Constants.Routes.COST_TIME.taskListHeading,
         href: Constants.Routes.COST_TIME.path,
         completedLabelId: 'cost-and-time-completed',
-        // rulesetId: Constants.RulesetIds.XXXXX,
-        // TODO
-        rulesetId: Constants.RulesetIds.CONFIRM_RULES,
-        available: false,
-        complete: false
+        rulesetId: Constants.RulesetIds.SHOW_COST_AND_TIME,
+        available: false
       }, {
         id: 'confirm-that-your-operation-meets-the-rules',
         label: Constants.Routes.CONFIRM_RULES.taskListHeading,
         href: Constants.Routes.CONFIRM_RULES.path,
         completedLabelId: 'operation-rules-completed',
         rulesetId: Constants.RulesetIds.CONFIRM_RULES,
-        available: false,
-        complete: false
+        available: false
       }]
     }
 
     const prepareToApplySection = {
-      sectionIndex: 2,
-      sectionName: 'Prepare to apply',
+      id: 'prepare-to-apply-section',
+      sectionNumber: 2,
+      sectionName: Constants.TaskList.SectionHeadings.PREPARE_TO_APPLY,
       sectionItems: [{
         id: 'tell-us-if-youve-discussed-this-application-with-us',
         label: Constants.Routes.PRE_APPLICATION.taskListHeading,
         href: Constants.Routes.PRE_APPLICATION.path,
         completedLabelId: 'preapp-completed',
         rulesetId: Constants.RulesetIds.PRE_APPLICATION,
-        available: false,
-        complete: true
+        available: false
       }]
     }
 
     const completeApplicationSection = {
-      sectionIndex: 3,
-      sectionName: 'Complete application',
+      id: 'complete-application-section',
+      sectionNumber: 3,
+      sectionName: Constants.TaskList.SectionHeadings.COMPLETE_APPLICATION,
       sectionItems: [{
         id: 'give-contact-details',
         label: Constants.Routes.CONTACT_DETAILS.taskListHeading,
         href: Constants.Routes.CONTACT_DETAILS.path,
         completedLabelId: 'contact-details-completed',
         rulesetId: Constants.RulesetIds.CONTACT_DETAILS,
-        available: false,
-        complete: false
+        available: false
       }, {
         id: 'give-permit-holder-details',
         label: Constants.Routes.PERMIT_HOLDER_TYPE.taskListHeading,
         href: Constants.Routes.PERMIT_HOLDER_TYPE.path,
         completedLabelId: 'site-operator-completed',
         rulesetId: Constants.RulesetIds.PERMIT_HOLDER_DETAILS,
-        available: false,
-        complete: false
+        available: false
       }, {
         id: 'give-site-name-and-location',
         label: Constants.Routes.SITE_SITE_NAME.taskListHeading,
         href: Constants.Routes.SITE_SITE_NAME.path,
         completedLabelId: 'site-name-completed',
-        // rulesetId: Constants.RulesetIds.XXXXX,
-        // TODO
-        rulesetId: Constants.RulesetIds.CONFIRM_RULES,
-        available: false,
-        complete: false
+        rulesetId: Constants.RulesetIds.SITE_NAME_LOCATION,
+        available: false
       }, {
         id: 'upload-the-site-plan',
         label: Constants.Routes.SITE_PLAN.taskListHeading,
         href: Constants.Routes.SITE_PLAN.path,
         completedLabelId: 'site-plan-completed',
         rulesetId: Constants.RulesetIds.SITE_PLAN,
-        available: false,
-        complete: false
+        available: false
       }, {
         id: 'upload-technical-management-qualifications',
         label: Constants.Routes.TECHNICAL_QUALIFICATION.taskListHeading,
         href: Constants.Routes.TECHNICAL_QUALIFICATION.path,
         completedLabelId: 'industry-scheme-completed',
         rulesetId: Constants.RulesetIds.TECHNICAL_QUALIFICATION,
-        available: false,
-        complete: false
+        available: false
       }, {
         id: 'tell-us-which-management-system-you-use',
         label: Constants.Routes.MANAGEMENT_SYSTEM.taskListHeading,
         href: Constants.Routes.MANAGEMENT_SYSTEM.path,
         completedLabelId: 'management-system-completed',
         rulesetId: Constants.RulesetIds.MANAGEMENT_SYSTEM,
-        available: false,
-        complete: false
+        available: false
       }, {
         id: 'upload-the-fire-prevention-plan',
         label: Constants.Routes.FIRE_PREVENTION_PLAN.taskListHeading,
         href: Constants.Routes.FIRE_PREVENTION_PLAN.path,
         completedLabelId: 'firepp-completed',
         rulesetId: Constants.RulesetIds.FIRE_PREVENTION_PLAN,
-        available: false,
-        complete: false
+        available: false
       }, {
         id: 'confirm-the-drainage-system-for-the-vehicle-storage-area',
         label: Constants.Routes.DRAINAGE_TYPE_DRAIN.taskListHeading,
         href: Constants.Routes.DRAINAGE_TYPE_DRAIN.path,
         completedLabelId: 'confirm-drainage-completed',
         rulesetId: Constants.RulesetIds.SURFACE_DRAINAGE,
-        available: false,
-        complete: false
+        available: false
       }, {
         id: 'confirm-confidentiality-needs',
         label: Constants.Routes.CONFIDENTIALITY.taskListHeading,
         href: Constants.Routes.CONFIDENTIALITY.path,
         completedLabelId: 'confidentiality-completed',
         rulesetId: Constants.RulesetIds.CONFIRM_CONFIDENTIALLY,
-        available: false,
-        complete: false
+        available: false
       }]
     }
 
     const sendAndPaySection = {
-      sectionIndex: 4,
-      sectionName: 'Send and pay',
+      id: 'send-and-pay-section',
+      sectionNumber: 4,
+      sectionName: Constants.TaskList.SectionHeadings.SEND_AND_PAY,
       sectionItems: [{
         id: 'submit-pay',
         label: Constants.Routes.CHECK_BEFORE_SENDING.taskListHeading,
         href: Constants.Routes.CHECK_BEFORE_SENDING.path,
         completedLabelId: 'submit-and-pay',
-        // rulesetId: Constants.RulesetIds.XXXXX,
-        // TODO
-        rulesetId: Constants.RulesetIds.CONFIRM_RULES,
-        available: false,
-        complete: false
+        // No ruleset ID - this item is Always present and not currently stored in Dynamics
+        available: true
       }]
     }
 
@@ -175,12 +161,19 @@ module.exports = class StandardRule extends BaseModel {
         sectionItem.available = rulesets[sectionItem.rulesetId]
       })
     })
+
+    // Set the final item (Send application and pay) to be always available
+    // Since this is always available this is currently not obtained from Dynamics
+    const finalSection = this.sections[this.sections.length - 1]
+    const finalItem = finalSection.sectionItems[finalSection.sectionItems.length - 1]
+    finalItem.available = true
   }
 
   static async getByCode (authToken, code) {
     const dynamicsDal = new DynamicsDalService(authToken)
 
     const standardRule = new StandardRule()
+    standardRule.defineStandardRuleSections()
 
     const filter = `defra_code eq '${code}'`
 
@@ -189,7 +182,6 @@ module.exports = class StandardRule extends BaseModel {
       `$expand=defra_wasteparametersId($select=${standardRule.getRulesetIds()})`)
     try {
       const response = await dynamicsDal.search(query)
-
       const result = response.value[0]
 
       // Set StandardRule properties
--- test/routes/taskList.route.test.js
+++ test/routes/taskList.route.test.js
@@ -1,17 +1,19 @@
 'use strict'
 
+const config = require('../../src/config/config')
+
 const Lab = require('lab')
 const lab = exports.lab = Lab.script()
 const Code = require('code')
 const DOMParser = require('xmldom').DOMParser
 
 const server = require('../../server')
-const StandardRule = require('../../src/models/standardRule.model')
+const DynamicsDalService = require('../../src/services/dynamicsDal.service')
 const CookieService = require('../../src/services/cookie.service')
 
 let generateCookieStub
 let validateCookieStub
-let standardRuleGetByCodeStub
+let dynamicsSearchStub
 
 let routePath = '/task-list'
 
@@ -19,52 +21,6 @@ const fakeCookie = {
   applicationId: 'my_application_id',
   authToken: 'my_auth_token'
 }
-
-// const fakeData = {
-//   'SR2015 No 18': [{
-//     sectionIndex: 1,
-//     sectionName: 'Before you apply',
-//     listItems: [{
-//       id: 'check-permit-cost-and-time',
-//       label: 'LABEL_1',
-//       href: 'HREF_1',
-//       completedLabelId: 'cost-and-time-completed',
-//       complete: true
-//     }, {
-//       id: 'confirm-that-your-operation-meets-the-rules',
-//       label: 'LABEL_2',
-//       href: 'HREF_2',
-//       completedLabelId: 'operation-rules-completed',
-//       complete: false
-//     }]
-//   }, {
-//     sectionIndex: 2,
-//     sectionName: 'Prepare to apply',
-//     listItems: [{
-//       id: 'tell-us-if-youve-discussed-this-application-with-us',
-//       label: 'LABEL_3',
-//       href: 'HREF_3',
-//       completedLabelId: 'preapp-completed',
-//       complete: true
-//     }]
-//   }, {
-//     sectionIndex: 3,
-//     sectionName: 'Complete application',
-//     listItems: [{
-//       id: 'give-contact-details',
-//       label: 'LABEL_4',
-//       href: 'HREF_4',
-//       completedLabelId: 'contact-details-completed',
-//       complete: false
-//     }, {
-//       id: 'give-permit-holder-details',
-//       label: 'LABEL_5',
-//       href: 'HREF_5',
-//       completedLabelId: 'site-operator-completed',
-//       complete: true
-//     }]
-//   }]
-// }
 
 lab.beforeEach((done) => {
   // Stub methods
@@ -78,13 +34,35 @@ lab.beforeEach((done) => {
     return true
   }
 
-  standardRuleGetByCodeStub = StandardRule.getByCode
-  StandardRule.getByCode = (authToken, code) => {
+  dynamicsSearchStub = DynamicsDalService.prototype.search
+  DynamicsDalService.prototype.search = (query) => {
+    // Dynamics StandardRule object
     return {
-      name: 'Metal recycling, vehicle storage, depollution and dismantling facility',
-      limits: 'Less than 25,000 tonnes a year of waste metal and less than 5,000 tonnes a year of waste motor vehicles',
-      code: 'SR2015 No 18',
-      codeForId: 'sr2015-no-18'
+      '@odata.context': `https://${config.dynamicsWebApiHost}${config.dynamicsWebApiPath}/$metadata#defra_standardrules(defra_rulesnamegovuk,defra_limits,defra_code,defra_wasteparametersId,defra_wasteparametersId(defra_showcostandtime,defra_confirmreadrules,defra_preapprequired,defra_contactdetailsrequired,defra_pholderdetailsrequired,defra_locationrequired,defra_siteplanrequired,defra_techcompetenceevreq,defra_mansystemrequired,defra_fireplanrequired,defra_surfacedrainagereq,defra_cnfconfidentialityreq))`,
+      value: [{
+        '@odata.etag': 'W/"1234567"',
+        defra_rulesnamegovuk: 'Metal recycling, vehicle storage, depollution and dismantling facility',
+        defra_limits: 'Less than 25,000 tonnes a year of waste metal and less than 5,000 tonnes a year of waste motor vehicles',
+        defra_code: 'SR2015 No 18',
+        defra_standardruleid: 'bd610c23-8ba7-e711-810a-5065f38a5b01',
+        defra_wasteparametersId: {
+          defra_showcostandtime: true,
+          defra_confirmreadrules: true,
+          defra_preapprequired: true,
+          defra_contactdetailsrequired: true,
+          defra_pholderdetailsrequired: true,
+          defra_locationrequired: true,
+
+          // Turn off the Upload Site Plan section
+          defra_siteplanrequired: false,
+
+          defra_techcompetenceevreq: true,
+          defra_mansystemrequired: true,
+          defra_fireplanrequired: true,
+          defra_surfacedrainagereq: true,
+          defra_cnfconfidentialityreq: true
+        }
+      }]
     }
   }
 
@@ -95,7 +73,7 @@ lab.afterEach((done) => {
   // Restore stubbed methods
   CookieService.generateCookie = generateCookieStub
   CookieService.validateCookie = validateCookieStub
-  StandardRule.prototype.getByCode = standardRuleGetByCodeStub
+  DynamicsDalService.prototype.search = dynamicsSearchStub
 
   done()
 })
@@ -111,6 +89,80 @@ lab.experiment('Task List page tests:', () => {
 
     server.inject(request, (res) => {
       Code.expect(res.statusCode).to.equal(200)
+      done()
+    })
+  })
+
+  lab.test('Task list contains the correct heading and StandardRule info', (done) => {
+    const request = {
+      method: 'GET',
+      url: routePath,
+      headers: {},
+      payload: {}
+    }
+
+    server.inject(request, (res) => {
+      Code.expect(res.statusCode).to.equal(200)
+
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(res.payload, 'text/html')
+
+      let element
+
+      // Check the existence of the page title and Standard Rule infos
+      element = doc.getElementById('task-list-heading')
+      Code.expect(element).to.exist()
+
+      element = doc.getElementById('task-list-heading-visually-hidden')
+      Code.expect(element).to.exist()
+
+      element = doc.getElementById('standard-rule-name-and-code')
+      Code.expect(element).to.exist()
+
+      element = doc.getElementById('select-a-different-permit')
+      Code.expect(element).to.exist()
+
+      done()
+    })
+  })
+
+  lab.test('Task list contains the correct section headings', (done) => {
+    const request = {
+      method: 'GET',
+      url: routePath,
+      headers: {},
+      payload: {}
+    }
+
+    server.inject(request, (res) => {
+      Code.expect(res.statusCode).to.equal(200)
+
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(res.payload, 'text/html')
+
+      let element
+
+      // Check the existence of the correct task list sections
+      element = doc.getElementById('before-you-apply-section-number')
+      Code.expect(element).to.exist()
+      element = doc.getElementById('before-you-apply-section-heading')
+      Code.expect(element).to.exist()
+
+      element = doc.getElementById('prepare-to-apply-section-number')
+      Code.expect(element).to.exist()
+      element = doc.getElementById('prepare-to-apply-section-heading')
+      Code.expect(element).to.exist()
+
+      element = doc.getElementById('complete-application-section-number')
+      Code.expect(element).to.exist()
+      element = doc.getElementById('complete-application-section-heading')
+      Code.expect(element).to.exist()
+
+      element = doc.getElementById('send-and-pay-section-number')
+      Code.expect(element).to.exist()
+      element = doc.getElementById('send-and-pay-section-heading')
+      Code.expect(element).to.exist()
+
       done()
     })
   })
@@ -157,21 +209,83 @@ lab.experiment('Task List page tests:', () => {
       element = doc.getElementById('give-permit-holder-details-link')
       Code.expect(element).to.exist()
 
-      // Check completed flags on task list items
-      element = doc.getElementById('cost-and-time-completed')
+      element = doc.getElementById('give-site-name-and-location')
       Code.expect(element).to.exist()
-      element = doc.getElementById('operation-rules-completed')
-      Code.expect(element).to.not.exist()
-      element = doc.getElementById('preapp-completed')
+      element = doc.getElementById('give-site-name-and-location-link')
       Code.expect(element).to.exist()
-      element = doc.getElementById('contact-details-completed')
+
+      // Site plan section should not exist
+      element = doc.getElementById('upload-the-site-plan')
       Code.expect(element).to.not.exist()
-      element = doc.getElementById('site-operator-completed')
+      element = doc.getElementById('upload-the-site-plan-link')
+      Code.expect(element).to.not.exist()
+
+      element = doc.getElementById('upload-technical-management-qualifications')
+      Code.expect(element).to.exist()
+      element = doc.getElementById('upload-technical-management-qualifications-link')
+      Code.expect(element).to.exist()
+
+      element = doc.getElementById('tell-us-which-management-system-you-use')
+      Code.expect(element).to.exist()
+      element = doc.getElementById('tell-us-which-management-system-you-use-link')
+      Code.expect(element).to.exist()
+
+      element = doc.getElementById('upload-the-fire-prevention-plan')
+      Code.expect(element).to.exist()
+      element = doc.getElementById('upload-the-fire-prevention-plan-link')
+      Code.expect(element).to.exist()
+
+      element = doc.getElementById('confirm-the-drainage-system-for-the-vehicle-storage-area')
+      Code.expect(element).to.exist()
+      element = doc.getElementById('confirm-the-drainage-system-for-the-vehicle-storage-area-link')
+      Code.expect(element).to.exist()
+
+      element = doc.getElementById('confirm-confidentiality-needs')
+      Code.expect(element).to.exist()
+      element = doc.getElementById('confirm-confidentiality-needs-link')
+      Code.expect(element).to.exist()
+
+      element = doc.getElementById('submit-pay')
+      Code.expect(element).to.exist()
+      element = doc.getElementById('submit-pay-link')
       Code.expect(element).to.exist()
 
       done()
     })
   })
+
+  // Completeness flags are not in scope yet
+  // lab.test('Task list items have the correct completeness flags', (done) => {
+  //   const request = {
+  //     method: 'GET',
+  //     url: routePath,
+  //     headers: {},
+  //     payload: {}
+  //   }
+  //
+  //   server.inject(request, (res) => {
+  //     Code.expect(res.statusCode).to.equal(200)
+  //
+  //     const parser = new DOMParser()
+  //     const doc = parser.parseFromString(res.payload, 'text/html')
+  //
+  //     let element
+  //
+  //     // Check completed flags on task list items
+  //     element = doc.getElementById('cost-and-time-completed')
+  //     Code.expect(element).to.exist()
+  //     element = doc.getElementById('operation-rules-completed')
+  //     Code.expect(element).to.not.exist()
+  //     element = doc.getElementById('preapp-completed')
+  //     Code.expect(element).to.exist()
+  //     element = doc.getElementById('contact-details-completed')
+  //     Code.expect(element).to.not.exist()
+  //     element = doc.getElementById('site-operator-completed')
+  //     Code.expect(element).to.exist()
+  //
+  //     done()
+  //   })
+  // })
 
   lab.test('GET /task-list redirects to error screen when the user token is invalid', (done) => {
     const request = {
