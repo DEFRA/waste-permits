e074e03d2038c46a9b8d4637ee401b477b66ac3a
Duncan Nixon <duncannixon@me.com>
2017-10-16 13:11:01 +0100

    Fix Standard.js errors.

--- src/assets/javascripts/application.js
+++ src/assets/javascripts/application.js
@@ -1,14 +1,14 @@
 /* global $ */
 
-$(window).on('load', function() {
+$(window).on('load', function () {
   // If there is an error summary, set the focus to the summary
   if ($('.error-summary').length) {
     $('.error-summary').focus()
   }
 })
 
-$(document).ready(function() {
+$(document).ready(function () {
   // Remove the no-js class if the user has Javascript available. This will show any elements
   // on the page that should only be visible when Javascript is available.
   $('.no-js').removeClass('no-js')
-});
+})
