19f6dbb8156f5aa823c2bc240e2d5e44dfe3262c
Duncan Nixon <duncannixon@me.com>
2017-11-20 16:51:30 +0000

    Unit testing (WIP).

---
+++ test/services/companyLookup.service.test.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Lab = require('lab')
+const lab = exports.lab = Lab.script()
+// const Code = require('code')
+// const nock = require('nock')
+
+// const CompanyLookupService = require('../../src/services/companyLookup.service')
+
+lab.beforeEach(() => {})
+
+lab.afterEach(() => {})
+
+lab.experiment('Company Lookup Service tests:', () => {
+  lab.test('getCompanyName() should return the correct company name', async () => {
+    // TODO
+  })
+})
