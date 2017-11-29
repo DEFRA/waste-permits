9ceeb984e8eab9439c093cf066097c492749f6c5
Ben Surgison <ajaxscape@users.noreply.github.com>
2017-11-29 11:51:33 +0000

    Declare bankruptcy or insolvency (#117)
    https://eaflood.atlassian.net/browse/WE-749 and https://eaflood.atlassian.net/browse/WE-746
    
    This is about the applicant declaring any bankruptcy or insolvency as part of the application.
    
    Controllers for "Declare bankruptcy or insolvency" and "Provide details of criminal convictions for a company" now inherit from a new base declarations controller as they share the majority of the code including error handling and the same view.

---
+++ src/routes/companyDeclareBankruptcy.route.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Constants = require('../constants')
+const {COMPANY_DECLARE_BANKRUPTCY, TASK_LIST} = Constants.Routes
+const CompanyDeclareBankruptcyController = require('../controllers/declareBankruptcy.controller')
+const CompanyDeclareBankruptcyValidator = require('../validators/declareBankruptcy.validator')
+const controller = new CompanyDeclareBankruptcyController(COMPANY_DECLARE_BANKRUPTCY, true, TASK_LIST, CompanyDeclareBankruptcyValidator)
+
+module.exports = [{
+  method: ['GET'],
+  path: controller.path,
+  config: {
+    description: `The GET Do you have current or past bankruptcy or insolvency proceedings to declare?`,
+    handler: controller.handler,
+    bind: controller
+  }
+}, {
+  method: ['POST'],
+  path: controller.path,
+  config: {
+    description: `The POST Do you have current or past bankruptcy or insolvency proceedings to declare?`,
+    handler: controller.handler,
+    bind: controller,
+    validate: {
+      options: {
+        allowUnknown: true
+      },
+      payload: controller.validator.getFormValidators(),
+      failAction: controller.failAction
+    }
+  }
+}]
