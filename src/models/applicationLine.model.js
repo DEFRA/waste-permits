cac89c1047a33279576eab587614abd9daebe236
Ben Surgison <ajaxscape@users.noreply.github.com>
2017-11-30 11:57:47 +0000

    Refactor of save method in models to prevent duplication (#118)
    * Refactor of save method in models to allow most of the common work to be done in the base model thus preventing code climate from triggering duplicate code alerts.
    
    * remove untilComplete from base model

--- src/models/applicationLine.model.js
+++ src/models/applicationLine.model.js
@@ -9,6 +9,7 @@ const Utilities = require('../utilities/utilities')
 module.exports = class ApplicationLine extends BaseModel {
   constructor (applicationLine) {
     super()
+    this.entity = 'defra_applicationlines'
     this.applicationId = applicationLine.applicationId
     this.standardRuleId = applicationLine.standardRuleId
     this.parametersId = applicationLine.parametersId
@@ -34,25 +35,11 @@ module.exports = class ApplicationLine extends BaseModel {
   }
 
   async save (authToken) {
-    const dynamicsDal = new DynamicsDalService(authToken)
-
     const dataObject = {
       'defra_applicationId@odata.bind': `defra_applications(${this.applicationId})`,
       'defra_standardruleId@odata.bind': `defra_standardrules(${this.standardRuleId})`,
       defra_permittype: Constants.Dynamics.PermitTypes.STANDARD
     }
-
-    try {
-      let query
-      if (this.isNew()) {
-        // New application line
-        query = 'defra_applicationlines'
-        this.id = await dynamicsDal.create(query, dataObject)
-        LoggingService.logInfo(`Created Application Line with ID: ${this.id}`)
-      }
-    } catch (error) {
-      LoggingService.logError(`Unable to save Application Line: ${error}`)
-      throw error
-    }
+    await super.save(authToken, dataObject)
   }
 }
