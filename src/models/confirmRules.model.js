6d5028101685872b60a3cb5e1075963933393a39
ben <ben@surgison.net>
2017-11-30 09:32:36 +0000

    The Dynamics Dal Service was not waiting for an asynchronous update to complete

--- src/models/confirmRules.model.js
+++ src/models/confirmRules.model.js
@@ -13,8 +13,6 @@ module.exports = class ConfirmRules extends BaseModel {
     this.applicationId = confirmRules.applicationId
     this.applicationLineId = confirmRules.applicationLineId
     this.complete = confirmRules.complete
-    // The following delay is required by the untilComplete method
-    this.delay = 250
     Utilities.convertFromDynamics(this)
   }
 
@@ -39,18 +37,6 @@ module.exports = class ConfirmRules extends BaseModel {
     }
   }
 
-  // A bug currently exists where the completeness isn't updated straight away in dynamics even when the save is successful.
-  // This function is a temporary fix to wait until we are sure we can get the completeness value.
-  // This and the code overriding the delay property in the test, can be removed when the update is successful only when the update in dynamics has fully completed.
-  async untilComplete (authToken) {
-    for (let retries = 10; retries && !(await ConfirmRules.getByApplicationId(authToken, this.applicationId, this.applicationLineId)).complete; retries--) {
-      if (!retries) {
-        throw new Error('Failed to complete')
-      }
-      await new Promise(resolve => setTimeout(resolve, this.delay))
-    }
-  }
-
   async save (authToken) {
     const dynamicsDal = new DynamicsDalService(authToken)
 
@@ -63,8 +49,6 @@ module.exports = class ConfirmRules extends BaseModel {
 
         const query = `defra_wasteparamses(${applicationLine.parametersId})`
         await dynamicsDal.update(query, entity)
-        // The following "untilComplete" can be removed when the update is successful only when the update in dynamics has fully completed.
-        await this.untilComplete(authToken)
       }
     } catch (error) {
       LoggingService.logError(`Unable to update Confirm Rules completeness: ${error}`)
