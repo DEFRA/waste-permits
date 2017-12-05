6e8a70d48ff9a483391239a9342ffad542fd846e
Duncan Nixon <duncannixon@users.noreply.github.com>
2017-12-05 18:57:49 +0000

    Bypassed the permit category route
    The purpose of this change is to bypass the Permit Category page. This is temporary as the page being bypassed is not currently part of the MVP. Unit tests have been amended accordingly.

--- test/routes/startOrOpenSaved.route.test.js
+++ test/routes/startOrOpenSaved.route.test.js
@@ -14,6 +14,14 @@ let validateCookieStub
 let applicationSaveStub
 
 const routePath = '/start/start-or-open-saved'
+const nextRoutePath = '/permit/select'
+
+const getRequest = {
+  method: 'GET',
+  url: routePath,
+  headers: {}
+}
+let postRequest
 
 const fakeCookie = {
   applicationId: 'my_application_id',
@@ -30,6 +38,12 @@ lab.beforeEach(() => {
 
   applicationSaveStub = Application.prototype.save
   Application.prototype.save = () => {}
+
+  postRequest = {
+    method: 'POST',
+    url: routePath,
+    headers: {}
+  }
 })
 
 lab.afterEach(() => {
@@ -40,103 +54,78 @@ lab.afterEach(() => {
 })
 
 lab.experiment('Start or Open Saved page tests:', () => {
-  lab.test('The page should NOT have a back link', async () => {
-    const request = {
-      method: 'GET',
-      url: routePath,
-      headers: {},
-      payload: {}
-    }
+  lab.experiment('General page tests:', () => {
+    lab.test('The page should NOT have a back link', async () => {
+      const res = await server.inject(getRequest)
+      Code.expect(res.statusCode).to.equal(200)
 
-    const res = await server.inject(request)
-    Code.expect(res.statusCode).to.equal(200)
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(res.payload, 'text/html')
 
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(res.payload, 'text/html')
-
-    let element = doc.getElementById('back-link')
-    Code.expect(element).to.not.exist()
+      let element = doc.getElementById('back-link')
+      Code.expect(element).to.not.exist()
+    })
   })
 
-  lab.test('GET returns the Start or Open Saved page correctly', async () => {
-    const request = {
-      method: 'GET',
-      url: routePath,
-      headers: {}
-    }
+  lab.experiment('GET:', () => {
+    lab.test('GET returns the Start or Open Saved page correctly', async () => {
+      const res = await server.inject(getRequest)
+      Code.expect(res.statusCode).to.equal(200)
 
-    const res = await server.inject(request)
-    Code.expect(res.statusCode).to.equal(200)
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(res.payload, 'text/html')
 
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(res.payload, 'text/html')
+      let element = doc.getElementById('page-heading').firstChild
+      Code.expect(element.nodeValue).to.equal('Apply for a standard rules waste permit')
 
-    let element = doc.getElementById('page-heading').firstChild
-    Code.expect(element.nodeValue).to.equal('Apply for a standard rules waste permit')
+      element = doc.getElementById('start-application')
+      Code.expect(element).to.exist()
 
-    element = doc.getElementById('start-application')
-    Code.expect(element).to.exist()
+      element = doc.getElementById('open-application')
+      Code.expect(element).to.exist()
 
-    element = doc.getElementById('open-application')
-    Code.expect(element).to.exist()
-
-    element = doc.getElementById('submit-button').firstChild
-    Code.expect(element.nodeValue).to.equal('Continue')
+      element = doc.getElementById('submit-button').firstChild
+      Code.expect(element.nodeValue).to.equal('Continue')
+    })
   })
 
-  lab.test('POST on Start or Open Saved page for a new application redirects to the correct route', async () => {
-    const request = {
-      method: 'POST',
-      url: routePath,
-      headers: {},
-      payload: {
+  lab.experiment('POST:', () => {
+    lab.test('POST on Start or Open Saved page for a new application redirects to the next route', async () => {
+      postRequest.payload = {
         'started-application': 'new'
       }
-    }
+      const res = await server.inject(postRequest)
+      Code.expect(res.statusCode).to.equal(302)
+      Code.expect(res.headers['location']).to.equal(nextRoutePath)
+    })
 
-    const res = await server.inject(request)
-    Code.expect(res.statusCode).to.equal(302)
-    Code.expect(res.headers['location']).to.equal('/permit/category')
-  })
-
-  lab.test('POST on Start or Open Saved page to open an existing application redirects to the correct route', async () => {
-    const request = {
-      method: 'POST',
-      url: routePath,
-      headers: {},
-      payload: {
+    lab.test('POST on Start or Open Saved page to open an existing application redirects to the correct route', async () => {
+      postRequest.payload = {
         'started-application': 'open'
       }
-    }
+      const res = await server.inject(postRequest)
+      Code.expect(res.statusCode).to.equal(302)
+      Code.expect(res.headers['location']).to.equal('/save-and-return/check-your-email')
+    })
 
-    const res = await server.inject(request)
-    Code.expect(res.statusCode).to.equal(302)
-    Code.expect(res.headers['location']).to.equal('/save-and-return/check-your-email')
-  })
+    lab.test('POST Start or Open Saved page shows the error message summary panel when new or open has not been selected', async () => {
+      postRequest.payload = {}
+      const res = await server.inject(postRequest)
+      Code.expect(res.statusCode).to.equal(200)
 
-  lab.test('POST Start or Open Saved page shows the error message summary panel when new or open has not been selected', async () => {
-    const request = {
-      method: 'POST',
-      url: routePath,
-      headers: {},
-      payload: {}
-    }
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(res.payload, 'text/html')
 
-    const res = await server.inject(request)
-    Code.expect(res.statusCode).to.equal(200)
+      let element
+      let errorMessage = 'Select start new or open a saved application'
 
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(res.payload, 'text/html')
+      // Panel summary error item
+      element = doc.getElementById('error-summary-list-item-0').firstChild
+      Code.expect(element.nodeValue).to.equal(errorMessage)
 
-    let element
-    let errorMessage = 'Select start new or open a saved application'
-
-    // Panel summary error item
-    element = doc.getElementById('error-summary-list-item-0').firstChild
-    Code.expect(element.nodeValue).to.equal(errorMessage)
-
-    // Site name field error
-    element = doc.getElementById('started-application-error').firstChild
-    Code.expect(element.nodeValue).to.equal(errorMessage)
+      // Site name field error
+      element = doc.getElementById('started-application-error').firstChild
+      Code.expect(element.nodeValue).to.equal(errorMessage)
+    })
   })
 })
