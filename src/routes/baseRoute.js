3b52127031526a7310be976a071c9dbdef3e96bc
ben <ben@surgison.net>
2018-01-02 16:52:02 +0000

    Satisfy code climate where constructor should be no more than 25 lines.

--- src/routes/baseRoute.js
+++ src/routes/baseRoute.js
@@ -3,36 +3,44 @@ const Merge = require('lodash.merge')
 class Route {
   constructor (routes, controller, validator) {
     const baseRoutes = {
-      GET: {
-        path: controller.path,
-        config: {
-          description: `The GET ${controller.route.pageHeading} page`,
-          handler: controller.handler,
-          bind: controller,
-          state: {
-            parse: true,
-            failAction: 'error'
-          }
-        }
-      },
-      POST: {
-        path: controller.path,
-        config: {
-          description: `The POST ${controller.route.pageHeading} page`,
-          handler: controller.handler,
-          bind: controller,
-          validate: {
-            options: {
-              allowUnknown: true
-            },
-            payload: validator ? validator.getFormValidators() : undefined,
-            failAction: controller.failAction
-          }
-        }
-      }
+      GET: Route._baseGet(controller),
+      POST: Route._basePost(controller, validator)
     }
 
     this.routes = routes.map((route) => Merge(baseRoutes[route.method], route))
+  }
+
+  static _baseGet (controller) {
+    return {
+      path: controller.path,
+      config: {
+        description: `The GET ${controller.route.pageHeading} page`,
+        handler: controller.handler,
+        bind: controller,
+        state: {
+          parse: true,
+          failAction: 'error'
+        }
+      }
+    }
+  }
+
+  static _basePost (controller, validator) {
+    return {
+      path: controller.path,
+      config: {
+        description: `The POST ${controller.route.pageHeading} page`,
+        handler: controller.handler,
+        bind: controller,
+        validate: {
+          options: {
+            allowUnknown: true
+          },
+          payload: validator ? validator.getFormValidators() : undefined,
+          failAction: controller.failAction
+        }
+      }
+    }
   }
 
   register () {
