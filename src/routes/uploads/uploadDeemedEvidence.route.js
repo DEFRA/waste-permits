0c6348009cda860490c1a1304120a79c47133979
ben <ben@surgison.net>
2018-01-04 12:39:46 +0000

    Make the upload routes consistent

--- src/routes/uploads/uploadDeemedEvidence.route.js
+++ src/routes/uploads/uploadDeemedEvidence.route.js
@@ -6,14 +6,14 @@ const {UPLOAD_DEEMED_EVIDENCE, TASK_LIST} = Constants.Routes
 const UploadDeemedEvidenceController = require('../../controllers/uploads/uploadDeemedEvidence.controller')
 const UploadEntityValidator = require('../../validators/uploadEntity.validator')
 
-const validatorOptions = {
+const validator = new UploadEntityValidator({
   maxSize: '30MB',
   fileTypes: [
     {type: 'PDF', mimeType: 'application/pdf'},
     {type: 'JPG', mimeType: 'image/jpeg'}
   ]
-}
+})
 
-const controller = new UploadDeemedEvidenceController(UPLOAD_DEEMED_EVIDENCE, true, TASK_LIST, new UploadEntityValidator(validatorOptions))
+const controller = new UploadDeemedEvidenceController(UPLOAD_DEEMED_EVIDENCE, true, TASK_LIST, validator)
 
 module.exports = Route.register('GET, POST, REMOVE, UPLOAD', controller)
--- src/routes/uploads/uploadEsaEuSkills.route.js
+++ src/routes/uploads/uploadEsaEuSkills.route.js
@@ -6,14 +6,14 @@ const {UPLOAD_ESA_EU_SKILLS, TASK_LIST} = Constants.Routes
 const UploadEsaEuSkillsController = require('../../controllers/uploads/uploadEsaEuSkills.controller')
 const UploadEntityValidator = require('../../validators/uploadEntity.validator')
 
-const validatorOptions = {
+const validator = new UploadEntityValidator({
   maxSize: '30MB',
   fileTypes: [
     {type: 'PDF', mimeType: 'application/pdf'},
     {type: 'JPG', mimeType: 'image/jpeg'}
   ]
-}
+})
 
-const controller = new UploadEsaEuSkillsController(UPLOAD_ESA_EU_SKILLS, true, TASK_LIST, new UploadEntityValidator(validatorOptions))
+const controller = new UploadEsaEuSkillsController(UPLOAD_ESA_EU_SKILLS, true, TASK_LIST, validator)
 
 module.exports = Route.register('GET, POST, REMOVE, UPLOAD', controller)
