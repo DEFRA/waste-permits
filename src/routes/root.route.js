84a9505b11981bd5eebb6e17848ad65d0278bee7
ben <ben@surgison.net>
2018-01-23 10:30:49 +0000

    Replace null parameters with undefined when instantiating a controller

--- src/routes/root.route.js
+++ src/routes/root.route.js
@@ -3,6 +3,6 @@
 const Constants = require('../constants')
 const Route = require('./baseRoute')
 const RootController = require('../controllers/root.controller')
-const controller = new RootController(Constants.Routes.ROOT, null, false)
+const controller = new RootController(Constants.Routes.ROOT, undefined, false)
 
 module.exports = Route.register('GET, POST', controller)
