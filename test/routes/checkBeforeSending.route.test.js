afb006f0184ba0a41981d69420c36737864930c8
ben <ben@surgison.net>
2018-01-31 15:13:42 +0000

    Declare application is correct

--- test/routes/checkBeforeSending.route.test.js
+++ test/routes/checkBeforeSending.route.test.js
@@ -6,68 +6,101 @@ const Code = require('code')
 const DOMParser = require('xmldom').DOMParser
 
 const server = require('../../server')
+
+const Application = require('../../src/models/application.model')
+const LoggingService = require('../../src/services/logging.service')
 const CookieService = require('../../src/services/cookie.service')
 
+let fakeApplication
+
 let validateCookieStub
+let logErrorStub
+let applicationGetByIdStub
+let applicationSaveStub
+
+let fakeApplicationId = 'APPLICATION_ID'
 
 const routePath = '/check-before-sending'
 
 lab.beforeEach(() => {
+  fakeApplication = {
+    id: fakeApplicationId,
+    declaration: true
+  }
+
   // Stub methods
   validateCookieStub = CookieService.validateCookie
   CookieService.validateCookie = () => true
+
+  logErrorStub = LoggingService.logError
+  LoggingService.logError = () => {}
+
+  applicationGetByIdStub = Application.getById
+  Application.getById = () => new Application(fakeApplication)
+
+  applicationSaveStub = Application.prototype.save
+  Application.prototype.save = () => {}
 })
 
 lab.afterEach(() => {
   // Restore stubbed methods
   CookieService.validateCookie = validateCookieStub
+  LoggingService.logError = logErrorStub
+  Application.getById = applicationGetByIdStub
+  Application.save = applicationSaveStub
 })
 
 lab.experiment('Check your answers before sending your application page tests:', () => {
-  lab.test('The page should have a back link', async () => {
-    const request = {
-      method: 'GET',
-      url: routePath,
-      headers: {},
-      payload: {}
-    }
+  lab.experiment(`GET ${routePath}`, () => {
+    let request
+    lab.beforeEach(() => {
+      request = {
+        method: 'GET',
+        url: routePath,
+        headers: {},
+        payload: {}
+      }
+    })
 
-    const res = await server.inject(request)
-    Code.expect(res.statusCode).to.equal(200)
+    lab.test('The page should have a back link', async () => {
+      const res = await server.inject(request)
+      Code.expect(res.statusCode).to.equal(200)
 
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(res.payload, 'text/html')
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(res.payload, 'text/html')
 
-    const element = doc.getElementById('back-link')
-    Code.expect(element).to.exist()
+      Code.expect(doc.getElementById('back-link')).to.exist()
+    })
+
+    lab.test('returns the check before sending page correctly', async () => {
+      const res = await server.inject(request)
+      Code.expect(res.statusCode).to.equal(200)
+
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(res.payload, 'text/html')
+
+      Code.expect(doc.getElementById('page-heading').firstChild.nodeValue).to.equal('Check your answers before sending your application')
+      Code.expect(doc.getElementById('submit-button').firstChild.nodeValue).to.equal('Confirm and pay')
+    })
   })
 
-  lab.test('GET /check-before-sending success ', async () => {
-    const request = {
-      method: 'GET',
-      url: routePath,
-      headers: {},
-      payload: {}
-    }
+  lab.experiment(`POST ${routePath}`, () => {
+    let request
+    lab.beforeEach(() => {
+      request = {
+        method: 'POST',
+        url: routePath,
+        headers: {},
+        payload: {}
+      }
+    })
 
-    const res = await server.inject(request)
-    Code.expect(res.statusCode).to.equal(200)
-  })
-
-  lab.test('GET /check-before-sending redirects to error screen when the user token is invalid', async () => {
-    const request = {
-      method: 'GET',
-      url: routePath,
-      headers: {},
-      payload: {}
-    }
-
-    CookieService.validateCookie = () => {
-      return undefined
-    }
-
-    const res = await server.inject(request)
-    Code.expect(res.statusCode).to.equal(302)
-    Code.expect(res.headers['location']).to.equal('/error')
+    lab.experiment('success', () => {
+      lab.test('redirects to the Application Received route after an UPDATE', async () => {
+        const res = await server.inject(request)
+        Code.expect(res.statusCode).to.equal(302)
+        Code.expect(res.headers['location']).to.equal('/done')
+      })
+    })
   })
 })
