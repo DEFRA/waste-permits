c2c392d7eb45cf9aeafacb22253d12a34c024bd3
ben <ben@surgison.net>
2018-02-13 09:23:19 +0000

    Upload POST routes no longer necessary as POST is included within the UPLOAD route.

--- src/routes/uploads/uploadEsaEuSkills.route.js
+++ src/routes/uploads/uploadEsaEuSkills.route.js
@@ -16,4 +16,4 @@ const validator = new UploadEntityValidator({
 
 const controller = new UploadEsaEuSkillsController(UPLOAD_ESA_EU_SKILLS, validator, true, TASK_LIST)
 
-module.exports = Route.register('GET, POST, REMOVE, UPLOAD', controller)
+module.exports = Route.register('GET, REMOVE, UPLOAD', controller)
