898c654bf486c55ffecb3a8922e474b35681e7f0
Ben Surgison <ajaxscape@users.noreply.github.com>
2018-02-19 14:32:31 +0000

    Postcode optional manual address component
    https://eaflood.atlassian.net/browse/WE-964

--- src/validators/address/addressManual.validator.js
+++ src/validators/address/addressManual.validator.js
@@ -17,7 +17,7 @@ const VALID_CHARACTERS_REGEX = /^[a-zA-Z0-9'\- ]*$/
 const VALID_CHARACTERS_REGEX_NO_NUMBERS = /^[a-zA-Z'\- ]*$/
 
 // More than one apostrophe is not allowed
-const MULTIPLE_APOSTROPHES_REGEX = /'/
+const MULTIPLE_APOSTROPHES_REGEX = /'/g
 
 const STARTS_OR_ENDS_WITH_HYPHEN_MESSAGE = `<FIELD> can’t start or end with a dash - please delete it`
 const INVALID_CHARS_ERROR_MESSAGE = `<FIELD> contains text we can’t accept - enter only numbers, letters, apostrophes, dashes and spaces`
@@ -54,8 +54,6 @@ module.exports = class AddressManualValidator extends BaseValidator {
         'custom.multiple.apostrophes': 'Town or city can only contain one apostrophe - remove all but one'
       },
       'postcode': {
-        'any.empty': `Enter a valid postcode`,
-        'any.required': `Enter a valid postcode`,
         'string.max': `Enter a shorter postcode with no more than ${POSTCODE_CHARACTER_LIMIT} characters`,
         'custom.starts.or.ends.with.hyphen': STARTS_OR_ENDS_WITH_HYPHEN_MESSAGE.replace('<FIELD>', `Postcode`)
       }
@@ -82,7 +80,6 @@ module.exports = class AddressManualValidator extends BaseValidator {
       'postcode': Joi
         .string()
         .max(POSTCODE_CHARACTER_LIMIT)
-        .required()
     }
   }
 
