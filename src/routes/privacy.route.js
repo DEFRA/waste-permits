2fa5b24ff5bc15a270d35ec8898eb226dd6e8d3f
Ben Surgison <ajaxscape@users.noreply.github.com>
2018-02-28 10:57:19 +0000

    Privacy page should be accessible when a permit has not been started

--- src/routes/privacy.route.js
+++ src/routes/privacy.route.js
@@ -3,6 +3,6 @@
 const Constants = require('../constants')
 const Route = require('./baseRoute')
 const PrivacyController = require('../controllers/privacy.controller')
-const controller = new PrivacyController(Constants.Routes.PRIVACY)
+const controller = new PrivacyController(Constants.Routes.PRIVACY, undefined, false)
 
 module.exports = Route.register('GET, POST', controller)
