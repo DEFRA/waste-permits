5261fa8a03d8d49825817efc643cd1a436d2821f
Duncan Nixon <duncannixon@me.com>
2018-03-01 17:34:21 +0000

    Removed commented out code

--- src/controllers/permitCategory.controller.js
+++ src/controllers/permitCategory.controller.js
@@ -2,21 +2,21 @@
 
 const Constants = require('../constants')
 const BaseController = require('./base.controller')
-// const CookieService = require('../services/cookie.service')
-// const Application = require('../models/application.model')
+const CookieService = require('../services/cookie.service')
+const Application = require('../models/application.model')
 
 module.exports = class PermitCategoryController extends BaseController {
   async doGet (request, reply, errors) {
     const pageContext = this.createPageContext(errors)
-    // const authToken = CookieService.get(request, Constants.COOKIE_KEY.AUTH_TOKEN)
-    // const applicationId = CookieService.get(request, Constants.COOKIE_KEY.APPLICATION_ID)
-    // const application = await Application.getById(authToken, applicationId)
+    const authToken = CookieService.get(request, Constants.COOKIE_KEY.AUTH_TOKEN)
+    const applicationId = CookieService.get(request, Constants.COOKIE_KEY.APPLICATION_ID)
+    const application = await Application.getById(authToken, applicationId)
 
-    // if (application.isSubmitted()) {
-    //   return reply
-    //     .redirect(Constants.Routes.ERROR.ALREADY_SUBMITTED.path)
-    //     .state(Constants.DEFRA_COOKIE_KEY, request.state[Constants.DEFRA_COOKIE_KEY], Constants.COOKIE_PATH)
-    // }
+    if (application.isSubmitted()) {
+      return reply
+        .redirect(Constants.Routes.ERROR.ALREADY_SUBMITTED.path)
+        .state(Constants.DEFRA_COOKIE_KEY, request.state[Constants.DEFRA_COOKIE_KEY], Constants.COOKIE_PATH)
+    }
 
     pageContext.formValues = request.payload
 
