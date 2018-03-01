ed2c3e515a918234cd8927dcb664061fd3caec3b
Duncan Nixon <duncannixon@me.com>
2018-03-01 17:30:29 +0000

    Removed commented out code.

--- src/controllers/error/notPaid.controller.js
+++ src/controllers/error/notPaid.controller.js
@@ -2,20 +2,13 @@
 
 const Constants = require('../../constants')
 const BaseController = require('../base.controller')
-// const CookieService = require('../../services/cookie.service')
-// const Application = require('../../models/application.model')
-// const ApplicationLine = require('../../models/applicationLine.model')
 
 module.exports = class NotPaidController extends BaseController {
   async doGet (request, reply, errors) {
     const pageContext = this.createPageContext(errors)
-    // const authToken = CookieService.get(request, Constants.COOKIE_KEY.AUTH_TOKEN)
-    // const applicationId = CookieService.get(request, Constants.COOKIE_KEY.APPLICATION_ID)
-    // const application = applicationId ? await Application.getById(authToken, applicationId) : undefined
-    // const applicationLineId = CookieService.get(request, Constants.COOKIE_KEY.APPLICATION_LINE_ID)
-    // const applicationLine = applicationLineId ? await ApplicationLine.getById(authToken, applicationLineId) : undefined
 
-    pageContext.payForApplicationRoute = 'TODO PAYMENT ROUTE HERE'
+    // TODO: We don't know that the payment route is yet
+    pageContext.payForApplicationRoute = 'UNKNOWN ROUTE'
     // pageContext.payForApplicationRoute = Constants.Routes.PAY_FOR_APPLICATION.path
 
     return reply
