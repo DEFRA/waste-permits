e70d39893bf80daec1392c6c2aadf4ab10e430ac
ben <ben@surgison.net>
2018-03-09 14:38:52 +0000

    Give drainage information

--- src/controllers/drainageTypeDrain.controller.js
+++ src/controllers/drainageTypeDrain.controller.js
@@ -2,21 +2,29 @@
 
 const Constants = require('../constants')
 const BaseController = require('./base.controller')
+const DrainageTypeDrain = require('../models/taskList/drainageTypeDrain.model')
 
 module.exports = class DrainageTypeDrainController extends BaseController {
   async doGet (request, reply, errors) {
     const pageContext = this.createPageContext(errors)
-    const {application} = await this.createApplicationContext(request, {application: true})
+    const {authToken, applicationLineId, application, standardRule} = await this.createApplicationContext(request, {application: true, standardRule: true})
 
     if (application.isSubmitted()) {
       return this.redirect(request, reply, Constants.Routes.ERROR.ALREADY_SUBMITTED.path)
     }
 
+    pageContext.guidanceUrl = standardRule.guidanceUrl
+    pageContext.code = standardRule.code
+    pageContext.isComplete = await DrainageTypeDrain.isComplete(authToken, applicationLineId)
+
     return this.showView(request, reply, 'drainageTypeDrain', pageContext)
   }
 
   async doPost (request, reply, errors) {
-    // Not implemented yet
-    return this.doGet(request, reply, errors)
+    const {authToken, applicationLineId} = await this.createApplicationContext(request)
+
+    await DrainageTypeDrain.updateCompleteness(authToken, applicationLineId)
+
+    return this.redirect(request, reply, Constants.Routes.TASK_LIST.path)
   }
 }
