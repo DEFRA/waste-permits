6dead3bd9646c5941136c0eb1c673e052da9f52c
Duncan Nixon <duncannixon@me.com>
2018-03-16 19:23:02 +0000

    Fixed bug in the isComplete calculation for Technical Qualification models.

--- src/models/taskList/technicalQualification.model.js
+++ src/models/taskList/technicalQualification.model.js
@@ -36,7 +36,7 @@ module.exports = class TechnicalQualification extends BaseModel {
     try {
       // Get the Evidence for a technical qualification
       const evidence = await Annotation.listByApplicationIdAndSubject(authToken, applicationId, Constants.UploadSubject.TECHNICAL_QUALIFICATION)
-      isComplete = Boolean(evidence.length)
+      isComplete = Boolean(evidence && evidence.length)
     } catch (error) {
       LoggingService.logError(`Unable to calculate TechnicalQualification completeness: ${error}`)
       throw error
