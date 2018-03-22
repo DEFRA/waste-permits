ead3917e80684dbb9ee58990f228e616ef94602b
Ben Surgison <ajaxscape@users.noreply.github.com>
2018-03-22 15:45:34 +0000

    Fix/we 968 cannot upload file more than 5mb

--- src/assets/javascripts/application.js
+++ src/assets/javascripts/application.js
@@ -7,6 +7,44 @@ $(window).on('load', function () {
   }
 })
 
+function uploadFilePanel () {
+  var pageBusy = false
+  var $uploadFilePanel = $('#upload-file-panel')
+
+  var setBusy = function () {
+    pageBusy = true
+    $uploadFilePanel.find('#submit-button').css('cursor', 'progress')
+    $uploadFilePanel.find('.remove-file-link').css('cursor', 'progress')
+    $uploadFilePanel.find('input').css('cursor', 'progress')
+  }
+
+  var skipIfBusy = function (e) {
+    if (pageBusy) {
+      e.preventDefault()
+    }
+  }
+
+  var onUpload = function (e) {
+    e.preventDefault()
+    if (!pageBusy) {
+      setBusy()
+      $('#uploadForm').submit()
+    }
+  }
+
+  var removeFile = function (e) {
+    e.preventDefault()
+    if (!pageBusy) {
+      setBusy()
+      window.location.href = $(this).attr('href')
+    }
+  }
+
+  $uploadFilePanel.find('#submit-button').click(onUpload)
+  $uploadFilePanel.find('.remove-file-link').click(removeFile)
+  $uploadFilePanel.find('input').click(skipIfBusy)
+}
+
 $(document).ready(function () {
   // Remove the no-js class if the user has Javascript available. This will show any elements
   // on the page that should only be visible when Javascript is available.
@@ -16,4 +54,7 @@ $(document).ready(function () {
   // Where .multiple-choice uses the data-target attribute to toggle hidden content
   var showHideContent = new GOVUK.ShowHideContent()
   showHideContent.init()
+
+  // don't allow user interaction while an upload or remove is in progress
+  uploadFilePanel()
 })
