8d7f80a7b2665dfef17ee1fc600eb0b02668f1b8
David Thomas <david.thomas@environment-agency.gov.uk>
2018-05-11 10:29:02 +0100

    Final changes to cookies messages

--- src/views/index.js
+++ src/views/index.js
@@ -23,7 +23,7 @@ const defaultContext = {
   bodyClasses: '',
   bodyStart: loadCommonPartial('bodyStart'),
   skipLinkMessage: '',
-  cookieMessage: '<p>We use cookies to store information about your actions on this site and to count visits. <a href="/information/cookies">Find out more about these cookies</a>.</p>',
+  cookieMessage: '<p>We use cookies to store an encrypted reference number, remember choices and help count visits. <a href="/information/cookies">Find out more about these cookies</a>.</p>',
   headerClass: 'with-proposition',
   homepageUrl: 'https://www.gov.uk',
   logoLinkTitle: 'Go to the GOV.UK homepage',
