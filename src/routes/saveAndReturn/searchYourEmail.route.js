d6dd594e1526f0a6a4628e2716e9aa9f1833c038
ben <ben@surgison.net>
2018-05-31 11:41:30 +0100

    Saved application page wrongly checks for application

--- src/routes/saveAndReturn/searchYourEmail.route.js
+++ src/routes/saveAndReturn/searchYourEmail.route.js
@@ -5,6 +5,6 @@ const Route = require('../baseRoute')
 const CheckYourEmailController = require('../../controllers/saveAndReturn/checkYourEmail.controller')
 const SaveAndReturnValidator = require('../../validators/saveAndReturn.validator')
 const validator = new SaveAndReturnValidator()
-const controller = new CheckYourEmailController({route: Constants.Routes.SEARCH_YOUR_EMAIL, validator, cookieValidationRequired: false})
+const controller = new CheckYourEmailController({route: Constants.Routes.SEARCH_YOUR_EMAIL, validator, cookieValidationRequired: false, applicationRequired: false})
 
 module.exports = Route.register('GET, POST', controller, validator)
