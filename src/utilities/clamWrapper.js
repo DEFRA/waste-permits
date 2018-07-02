d10b1c1752ca95236a55d60b5b798826c6282e99
Ben Surgison <ajaxscape@users.noreply.github.com>
2018-07-02 13:41:02 +0100

    Allow the virus scan to use all available cores

--- src/utilities/clamWrapper.js
+++ src/utilities/clamWrapper.js
@@ -12,7 +12,7 @@ module.exports = class ClamWrapper {
   // Lazily instantiate clam so that the tests work without ClamAV installed
   static _init () {
     if (!isInfected) {
-      const clam = clamscan({clamdscan: {config_file: '/etc/clamav/clamd.conf', reload_db: false, active: true}})
+      const clam = clamscan({clamdscan: {config_file: '/etc/clamav/clamd.conf', multiscan: true, reload_db: false, active: true}})
 
       const {promisify} = require('es6-promisify')
 
