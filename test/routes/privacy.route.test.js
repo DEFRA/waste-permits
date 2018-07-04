c2541133e6f2360ec5f1927b91310b2b85894aaf
ben <ben@surgison.net>
2018-07-04 16:54:34 +0100

    Update privacy page text

--- test/routes/privacy.route.test.js
+++ test/routes/privacy.route.test.js
@@ -44,24 +44,32 @@ lab.experiment('Privacy page tests:', () => {
 
     // Test for the existence of expected static content
     GeneralTestHelper.checkElementsExist(doc, [
+      'personal-information-heading-1',
+      'personal-information-heading-2',
+      'personal-information-heading-3',
+      'personal-information-heading-4',
       'personal-information-charter-paragraph-1',
       'personal-information-charter-paragraph-2',
-      'personal-information-charter-paragraph-3',
+      'personal-information-charter-paragraph-4',
+      'personal-information-charter-paragraph-5',
+      'personal-information-charter-paragraph-6',
+      'personal-information-charter-paragraph-7',
+      'personal-information-charter-paragraph-8',
+      'personal-information-charter-paragraph-9',
+      'personal-information-charter-paragraph-10',
+      'personal-information-charter-paragraph-11',
+      'personal-information-charter-paragraph-12',
+      'personal-information-charter-paragraph-13',
       'personal-information-charter-link',
-      'public-register-information',
-      'public-register-information-item-1',
-      'public-register-information-item-2',
-      'public-register-information-item-3',
       'personal-information',
       'personal-information-item-1',
       'personal-information-item-2',
       'personal-information-item-3',
       'personal-information-item-4',
-      'personal-information-item-5',
-      'personal-information-item-6',
-      'sharing-information'
+      'personal-information-item-5'
     ])
 
     Code.expect(doc.getElementById('personal-information-charter-link').getAttribute('href')).to.equal('https://www.gov.uk/government/organisations/environment-agency/about/personal-information-charter')
+    Code.expect(doc.getElementById('cookies-link').getAttribute('href')).to.equal('/information/cookies')
   })
 })
