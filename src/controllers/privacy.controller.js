499000b6570e4a760f43918a669bb6997ee212b2
Ben Surgison <ajaxscape@users.noreply.github.com>
2018-07-05 09:46:24 +0100

    Update privacy page text
    https://eaflood.atlassian.net/browse/WE-1223

--- src/controllers/privacy.controller.js
+++ src/controllers/privacy.controller.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const BaseController = require('./base.controller')
+const {COOKIES} = require('../routes')
 
 module.exports = class PrivacyController extends BaseController {
   async doGet (request, h, errors) {
     const pageContext = this.createPageContext(errors)
+    pageContext.cookieRoute = COOKIES.path
     return this.showView({request, h, pageContext})
   }
 }
