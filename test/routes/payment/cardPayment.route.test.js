f101364cf0b67dca7142caf52f5143be07aaf95a
ben <ben@surgison.net>
2018-08-13 15:47:35 +0100

    Handle the failure of gov pay and give the user a chance to pay by bacs

--- test/routes/payment/cardPayment.route.test.js
+++ test/routes/payment/cardPayment.route.test.js
@@ -19,8 +19,12 @@ const {COOKIE_RESULT} = require('../../../src/constants')
 
 let sandbox
 
+const slug = 'SLUG'
+const paymentErrorStatus = 'error'
+
 const routePath = `/pay/card`
 const errorPath = '/errors/technical-problem'
+const cardProblemPath = `/pay/card-problem/${slug}?status=${paymentErrorStatus}`
 const notSubmittedRoutePath = '/errors/order/check-answers-not-complete'
 const returnFromGovPayUrl = '/return/from/govr/pay/url'
 
@@ -28,6 +32,7 @@ let fakeApplication
 let fakeApplicationLine
 let fakeApplicationReturn
 let fakePayment
+let fakePaymentResult
 let fakeStandardRule
 let fakeRecovery
 
@@ -50,6 +55,10 @@ lab.beforeEach(() => {
     returnUrl: returnFromGovPayUrl
   }
 
+  fakePaymentResult = {
+    PaymentNextUrlHref: returnFromGovPayUrl
+  }
+
   fakeStandardRule = {
     permitName: 'STANDARD_RULE_NAME',
     code: 'SR2015 No 18'
@@ -62,7 +71,8 @@ lab.beforeEach(() => {
     application: new Application(fakeApplication),
     applicationLine: new ApplicationLine(fakeApplicationLine),
     applicationReturn: new ApplicationReturn(fakeApplicationReturn),
-    standardRule: new StandardRule(fakeStandardRule)
+    standardRule: new StandardRule(fakeStandardRule),
+    slug: 'SLUG'
   })
 
   // Create a sinon sandbox to stub methods
@@ -71,7 +81,7 @@ lab.beforeEach(() => {
   // Stub methods
   sandbox.stub(Application.prototype, 'isSubmitted').value(() => fakeApplication.submitted)
   sandbox.stub(Payment.prototype, 'save').value(() => fakePayment.id)
-  sandbox.stub(Payment.prototype, 'makeCardPayment').value(() => fakePayment.returnUrl)
+  sandbox.stub(Payment.prototype, 'makeCardPayment').value(() => fakePaymentResult)
   sandbox.stub(Payment, 'getCardPaymentDetails').value(() => new Payment(fakePayment))
   sandbox.stub(CookieService, 'validateCookie').value(() => COOKIE_RESULT.VALID_COOKIE)
   sandbox.stub(RecoveryService, 'createApplicationContext').value(() => fakeRecovery())
@@ -107,6 +117,13 @@ lab.experiment(`How do you want to pay?:`, () => {
         const res = await server.inject(getRequest)
         Code.expect(res.statusCode).to.equal(302)
         Code.expect(res.headers['location']).to.equal(returnFromGovPayUrl)
+      })
+
+      lab.test('redirects to card problem when status is error', async () => {
+        fakePaymentResult.PaymentStatus = paymentErrorStatus
+        const res = await server.inject(getRequest)
+        Code.expect(res.statusCode).to.equal(302)
+        Code.expect(res.headers['location']).to.equal(cardProblemPath)
       })
 
       lab.test('redirects when application not submitted', async () => {
