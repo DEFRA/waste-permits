cc7f34ca82c56a81303576c327b319dd0a579f3a
Ben Surgison <ajaxscape@users.noreply.github.com>
2018-09-06 09:11:31 +0100

    Create recovery code route for development only
    https://eaflood.atlassian.net/browse/WE-1389

--- src/config/featureConfig.js
+++ src/config/featureConfig.js
@@ -2,5 +2,6 @@ const config = {}
 
 // Add feature switches here such as the example hasLLPFeature below
 // config.hasLLPFeature = process.env.LLP_FEATURE === 'TRUE' || false
+config.hasDisplayRecoveryLinkFeature = process.env.DISPLAY_RECOVERY_LINK_FEATURE === 'TRUE' || false
 
 module.exports = config
