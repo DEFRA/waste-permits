6d0290560b42cb7e45ebd01d458034a9ad652e37
Ben Sagar <benjamin.sagar@environment-agency.gov.uk>
2018-12-12 15:50:07 +0000

    Consultee and management system fixes
    Content changes and error handling

--- src/validators/needToConsult.validator.js
+++ src/validators/needToConsult.validator.js
@@ -7,7 +7,7 @@ const MAX_NAME_LENGTH = 150
 module.exports = class NeedToConsultValidator extends BaseValidator {
   get errorMessages () {
     return {
-      'consult-none-required': {
+      'consult-select': {
         'object.without': `You cannot select a release and 'None of these'. Please deselect one of them.`,
         'object.missing': `Select at least one option. If there are no releases select 'None of these'.`
       },
@@ -27,31 +27,33 @@ module.exports = class NeedToConsultValidator extends BaseValidator {
   }
 
   get formValidators () {
-    return Joi.object().keys({
-      'consult-sewerage-undertaker': Joi
-        .string()
-        .max(MAX_NAME_LENGTH)
-        .when('consult-sewer-required', {
-          is: 'yes',
-          then: Joi.required(),
-          otherwise: Joi.optional() }),
-      'consult-harbour-authority': Joi
-        .string()
-        .max(MAX_NAME_LENGTH)
-        .when('consult-harbour-required', {
-          is: 'yes',
-          then: Joi.required(),
-          otherwise: Joi.optional() }),
-      'consult-fisheries-committee': Joi
-        .string()
-        .max(MAX_NAME_LENGTH)
-        .when('consult-fisheries-required', {
-          is: 'yes',
-          then: Joi.required(),
-          otherwise: Joi.optional() })
-    })
+    return Joi.object()
+      .keys({
+        'consult-none-required': Joi.string().optional().label('consult-select'),
+        'consult-sewerage-undertaker': Joi
+          .string()
+          .max(MAX_NAME_LENGTH)
+          .when('consult-sewer-required', {
+            is: 'yes',
+            then: Joi.required(),
+            otherwise: Joi.optional() }),
+        'consult-harbour-authority': Joi
+          .string()
+          .max(MAX_NAME_LENGTH)
+          .when('consult-harbour-required', {
+            is: 'yes',
+            then: Joi.required(),
+            otherwise: Joi.optional() }),
+        'consult-fisheries-committee': Joi
+          .string()
+          .max(MAX_NAME_LENGTH)
+          .when('consult-fisheries-required', {
+            is: 'yes',
+            then: Joi.required(),
+            otherwise: Joi.optional() })
+      })
       .without('consult-none-required', ['consult-sewer-required', 'consult-harbour-required', 'consult-fisheries-required'])
       .or('consult-none-required', 'consult-sewer-required', 'consult-harbour-required', 'consult-fisheries-required')
-      .label('consult-none-required') // Label with the name of the field that the error should be attached to on the form
+      .label('consult-select') // Label with the name of the field that the error should be attached to on the form
   }
 }
