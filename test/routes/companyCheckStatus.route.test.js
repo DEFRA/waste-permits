3733f39e3faa930e9ed273b96458f1d243b14072
ben <ben@surgison.net>
2019-01-21 09:47:09 +0000

    Use new mocks for company check status and company check type unit tests

--- test/routes/companyCheckStatus.route.test.js
+++ test/routes/companyCheckStatus.route.test.js
@@ -4,6 +4,7 @@ const Lab = require('lab')
 const lab = exports.lab = Lab.script()
 const Code = require('code')
 const sinon = require('sinon')
+const Mocks = require('../helpers/mocks')
 const GeneralTestHelper = require('./generalTestHelper.test')
 
 const server = require('../../server')
@@ -11,7 +12,7 @@ const CookieService = require('../../src/services/cookie.service')
 const CompanyLookupService = require('../../src/services/companyLookup.service')
 const Account = require('../../src/persistence/entities/account.entity')
 const Application = require('../../src/persistence/entities/application.entity')
-const CharityDetail = require('../../src/models/charityDetail.model')
+const RecoveryService = require('../../src/services/recovery.service')
 const LoggingService = require('../../src/services/logging.service')
 
 const { COOKIE_RESULT } = require('../../src/constants')
@@ -25,12 +26,6 @@ const COMPANY_STATUSES = {
   INSOLVENCY_PROCEEDINGS: 'is insolvent',
   NOT_ACTIVE: `is not active`
 }
-
-let sandbox
-
-let fakeApplication
-let fakeAccount
-let fakeCompany
 
 const routes = {
   'Limited Company': {
@@ -51,36 +46,23 @@ const routes = {
 
 Object.entries(routes).forEach(([companyType, { pageHeading, routePath, nextPath, errorPath, requiredOfficers }]) => {
   lab.experiment(companyType, () => {
+    let mocks
+    let sandbox
+
     lab.beforeEach(() => {
-      fakeApplication = {
-        id: 'APPLICATION_ID',
-        applicationNumber: 'APPLICATION_NUMBER'
-      }
-
-      fakeAccount = {
-        id: 'ACCOUNT_ID',
-        companyNumber: '01234567'
-      }
-
-      fakeCompany = {
-        name: 'THE COMPANY NAME',
-        address: 'THE COMPANY ADDRESS',
-        status: 'NOT_ACTIVE',
-        isActive: false
-      }
+      mocks = new Mocks()
 
       // Create a sinon sandbox to stub methods
       sandbox = sinon.createSandbox()
 
       // Stub methods
       sandbox.stub(CookieService, 'validateCookie').value(() => COOKIE_RESULT.VALID_COOKIE)
-      sandbox.stub(CompanyLookupService, 'getCompany').value(() => fakeCompany)
+      sandbox.stub(CompanyLookupService, 'getCompany').value(() => mocks.companyData)
       sandbox.stub(CompanyLookupService, 'getActiveOfficers').value(() => [{}])
-      sandbox.stub(Account, 'getByApplicationId').value(() => fakeAccount)
-      sandbox.stub(Application, 'getById').value(() => new Application(fakeApplication))
+      sandbox.stub(Application, 'getById').value(() => mocks.application)
       sandbox.stub(Application.prototype, 'isSubmitted').value(() => false)
-      sandbox.stub(Account.prototype, 'save').value(() => fakeAccount)
-      sandbox.stub(CharityDetail, 'get').value(() => new CharityDetail({}))
+      sandbox.stub(Account.prototype, 'save').value(() => undefined)
+      sandbox.stub(RecoveryService, 'createApplicationContext').value(() => mocks.recovery)
     })
 
     lab.afterEach(() => {
@@ -116,13 +98,13 @@ Object.entries(routes).forEach(([companyType, { pageHeading, routePath, nextPath
           lab.experiment('for company with a status of', () => {
             Object.keys(COMPANY_STATUSES).forEach((status) => {
               lab.test(`${status}`, async () => {
-                fakeCompany.status = status
-                fakeCompany.isActive = (status === 'ACTIVE')
+                mocks.companyData.status = status
+                mocks.companyData.isActive = (status === 'ACTIVE')
                 const doc = await GeneralTestHelper.getDoc(getRequest)
                 Code.expect(doc.getElementById('page-heading').firstChild.nodeValue).to.equal(`${pageHeading} ${COMPANY_STATUSES[status]}`)
                 Code.expect(doc.getElementById('company-status-message')).to.exist()
-                Code.expect(doc.getElementById('company-name').firstChild.nodeValue).to.equal(fakeCompany.name)
-                Code.expect(doc.getElementById('company-number').firstChild.nodeValue).to.equal(fakeAccount.companyNumber)
+                Code.expect(doc.getElementById('company-name').firstChild.nodeValue).to.equal(mocks.companyData.name)
+                Code.expect(doc.getElementById('company-number').firstChild.nodeValue).to.equal(mocks.account.companyNumber)
               })
             })
           })
@@ -130,25 +112,25 @@ Object.entries(routes).forEach(([companyType, { pageHeading, routePath, nextPath
           lab.experiment('for company with', () => {
             lab.test(`no ${requiredOfficers}`, async () => {
               CompanyLookupService.getActiveOfficers = () => []
-              fakeCompany.isActive = true
+              mocks.companyData.isActive = true
               const doc = await GeneralTestHelper.getDoc(getRequest)
               Code.expect(doc.getElementById('page-heading').firstChild.nodeValue).to.equal(`${pageHeading} has no ${requiredOfficers}`)
               Code.expect(doc.getElementById('company-status-message')).to.exist()
-              Code.expect(doc.getElementById('company-name').firstChild.nodeValue).to.equal(fakeCompany.name)
-              Code.expect(doc.getElementById('company-number').firstChild.nodeValue).to.equal(fakeAccount.companyNumber)
+              Code.expect(doc.getElementById('company-name').firstChild.nodeValue).to.equal(mocks.companyData.name)
+              Code.expect(doc.getElementById('company-number').firstChild.nodeValue).to.equal(mocks.account.companyNumber)
             })
           })
 
           lab.test('for company with a status of ACTIVE', async () => {
-            fakeCompany.status = 'ACTIVE'
-            fakeCompany.isActive = true
+            mocks.companyData.status = 'ACTIVE'
+            mocks.companyData.isActive = true
             const res = await server.inject(getRequest)
             Code.expect(res.statusCode).to.equal(302)
             Code.expect(res.headers['location']).to.equal(nextPath)
           })
 
           lab.test('for a company that does not exist', async () => {
-            fakeCompany = {}
+            delete mocks.companyData.status
             const res = await server.inject(getRequest)
             Code.expect(res.statusCode).to.equal(302)
             Code.expect(res.headers['location']).to.equal(nextPath)
@@ -156,10 +138,10 @@ Object.entries(routes).forEach(([companyType, { pageHeading, routePath, nextPath
         })
 
         lab.experiment('failure', () => {
-          lab.test('redirects to error screen when failing to get the application ID', async () => {
+          lab.test('redirects to error screen when failing to recover the application', async () => {
             const spy = sandbox.spy(LoggingService, 'logError')
-            Account.getByApplicationId = () => {
-              throw new Error('read failed')
+            RecoveryService.createApplicationContext = () => {
+              throw new Error('application recovery failed')
             }
 
             const res = await server.inject(getRequest)
