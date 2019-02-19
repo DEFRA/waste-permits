ce8faec3435ce684a4a9142f050eb139f805a3d7
David Harris <davidharriswork@gmail.com>
2019-02-19 16:46:03 +0000

    WE-2134 Implement template download screen

--- src/controllers/mcpTemplate.controller.js
+++ src/controllers/mcpTemplate.controller.js
@@ -3,11 +3,28 @@
 const BaseController = require('./base.controller')
 const McpTemplate = require('../models/taskList/mcpTemplate.task')
 const RecoveryService = require('../services/recovery.service')
+const DataStore = require('../models/dataStore.model')
+const Constants = require('../constants')
+const { BESPOKE: { id: BESPOKE }, STANDARD_RULES: { id: STANDARD_RULES } } = Constants.PermitTypes
 
 module.exports = class McpTemplateController extends BaseController {
   async doGet (request, h, errors) {
     const pageContext = this.createPageContext(h, errors)
     const context = await RecoveryService.createApplicationContext(h)
+
+    // Check the permit type (standard rules vs bespoke) and set a flag for the view
+    // (I didn't pass the permitType value into the view because it would mean hardcoding the permitType values (set in constants.js) in the view)
+    const { data: { permitType } } = await DataStore.get(context)
+    switch (permitType) {
+      case STANDARD_RULES:
+        pageContext.standardPermitType = true
+        break
+      case BESPOKE:
+        pageContext.bespokePermitType = true
+        break
+      default:
+        throw new Error(`Unexpected permitType: ${permitType}`)
+    }
 
     pageContext.isComplete = await McpTemplate.isComplete(context)
 
