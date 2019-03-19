a7d008f279b4875f05a6d977191301bec053749d
rudenoise <rudenoise@gmail.com>
2019-03-19 15:50:59 +0000

    WE-2200: add checko and redirect for mobile permit

--- src/controllers/existingPermit.controller.js
+++ src/controllers/existingPermit.controller.js
@@ -4,9 +4,22 @@ const BaseController = require('./base.controller')
 const DataStore = require('../models/dataStore.model')
 const RecoveryService = require('../services/recovery.service')
 const { BESPOKE: { id: BESPOKE } } = require('../../src/constants').PermitTypes
+const { MCP_TYPES: { MOBILE_SG } } = require('../models/triage/triageLists')
 
 module.exports = class ExistingPermitController extends BaseController {
   async doGet (request, h, errors) {
+    const context = await RecoveryService.createApplicationContext(h)
+    const dataStore = await DataStore.get(context)
+    const data = dataStore.data
+    // if the mcp type is mobile:
+    //   no need to worry about existing application
+    //   so jump to air dispersion steps
+    if (
+      data.permitType === BESPOKE &&
+      data.mcpType === MOBILE_SG.id
+    ) {
+      return this.redirect({ h, route: 'MCP_AIR_DISPERSION_MODELLING' })
+    }
     const pageContext = this.createPageContext(h, errors)
 
     return this.showView({ h, pageContext })
