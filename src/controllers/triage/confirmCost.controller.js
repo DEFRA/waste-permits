f055b7ef4d4c484d28a6bba2192c18ae016ef4a3
rudenoise <rudenoise@gmail.com>
2019-04-02 13:52:30 +0100

    WE-2327: remove permit holder step from bespoke flow

--- src/controllers/triage/confirmCost.controller.js
+++ src/controllers/triage/confirmCost.controller.js
@@ -2,11 +2,8 @@
 
 const { DEFRA_COOKIE_KEY, COOKIE_KEY: { APPLICATION_ID } } = require('../../constants')
 
-const { TRIAGE_WASTE_ACTIVITY } = require('../../routes')
-
 const BaseController = require('../base.controller')
 const ApplicationCost = require('../../models/triage/applicationCost.model')
-const Application = require('../../models/triage/application.model')
 
 const ActiveDirectoryAuthService = require('../../services/activeDirectoryAuth.service')
 const authService = new ActiveDirectoryAuthService()
@@ -18,10 +15,6 @@ module.exports = class ConfirmCostController extends BaseController {
     entityContext.applicationId = request.state[DEFRA_COOKIE_KEY] ? request.state[DEFRA_COOKIE_KEY][APPLICATION_ID] : undefined
 
     pageContext.calculatedCosts = await ApplicationCost.getApplicationCostForApplicationId(entityContext)
-
-    // Determine triage path for page
-    const permitHolderType = await Application.getPermitHolderTypeForApplicationId(entityContext)
-    pageContext.wasteActivitiesLink = `${TRIAGE_WASTE_ACTIVITY.path}/bespoke/${permitHolderType.id}`
 
     return this.showView({ h, pageContext })
   }
