b6425e86134a34396c61a4ff913d6bf6047e3f0e
ben <ben@surgison.net>
2019-04-08 11:19:59 +0100

    Link download page to spreadsheets

--- src/controllers/mcpTemplate.controller.js
+++ src/controllers/mcpTemplate.controller.js
@@ -12,39 +12,42 @@ let templatesStandardRules = [
   { id: 'mcp-template-ods-link', name: 'Plant or generator list template (Open Document ODS)', file: 'mcp-plant-generator-list-template-v0-1.ods' }
 ]
 let templatesBespokeAppendix1 = [
-  { id: 'mcp-template-xls-link', name: 'Plant or generator list template APPENDIX 1 (Excel XLS)', file: 'todo' },
-  { id: 'mcp-template-ods-link', name: 'Plant or generator list template APPENDIX 1 (Open Document ODS)', file: 'todo' }
+  { id: 'mcp-template-xls-link', name: 'Plant or generator list template APPENDIX 1 (Excel XLS)', file: 'plant-generator-list-template-app1-v1-0.xls' },
+  { id: 'mcp-template-ods-link', name: 'Plant or generator list template APPENDIX 1 (Open Document ODS)', file: 'plant-generator-list-template-app1-v1-0.ods' }
 ]
 let templatesBespokeAppendix2 = [
-  { id: 'mcp-template-xls-link', name: 'Plant or generator list template APPENDIX 2 (Excel XLS)', file: 'todo' },
-  { id: 'mcp-template-ods-link', name: 'Plant or generator list template APPENDIX 2 (Open Document ODS)', file: 'todo' }
+  { id: 'mcp-template-xls-link', name: 'Plant or generator list template APPENDIX 2 (Excel XLS)', file: 'plant-generator-list-template-app1-v2-0.xls' },
+  { id: 'mcp-template-ods-link', name: 'Plant or generator list template APPENDIX 2 (Open Document ODS)', file: 'plant-generator-list-template-app1-v2-0.ods' }
 ]
 
 module.exports = class McpTemplateController extends BaseController {
+  async getTemplates (context) {
+    // There are different templates, so show the template downloads relevant to this application
+    const { data: { permitType, mcpType } } = await DataStore.get(context)
+    switch (permitType) {
+      case STANDARD_RULES.id:
+        return templatesStandardRules
+      case BESPOKE.id:
+        switch (mcpType) {
+          case STATIONARY_MCP.id:
+          case STATIONARY_MCP_AND_SG.id:
+          case MOBILE_SG_AND_MCP:
+            return templatesBespokeAppendix1
+          case STATIONARY_SG.id:
+          case MOBILE_SG.id:
+            return templatesBespokeAppendix2
+          default:
+            throw new Error(`Unexpected mcpType: ${mcpType}`)
+        }
+      default:
+        throw new Error(`Unexpected permitType: ${permitType}`)
+    }
+  }
+
   async doGet (request, h, errors) {
     const pageContext = this.createPageContext(h, errors)
     const context = await RecoveryService.createApplicationContext(h)
-
-    // There are different templates, so show the template downloads relevant to this application
-    const { data: { permitType, mcpType } } = await DataStore.get(context)
-    if (permitType === STANDARD_RULES.id) {
-      pageContext.templates = templatesStandardRules
-    } else if (permitType === BESPOKE.id) {
-      // TODO: Set the correct links and put the linked files in place, ready for download
-      if (mcpType === STATIONARY_MCP.id ||
-          mcpType === STATIONARY_MCP_AND_SG.id ||
-          mcpType === MOBILE_SG_AND_MCP.id) {
-        pageContext.templates = templatesBespokeAppendix1
-      } else if (mcpType === STATIONARY_SG.id ||
-                 mcpType === MOBILE_SG.id) {
-        pageContext.templates = templatesBespokeAppendix2
-      } else {
-        throw new Error(`Unexpected mcpType: ${mcpType}`)
-      }
-    } else {
-      throw new Error(`Unexpected permitType: ${permitType}`)
-    }
-
+    pageContext.templates = await this.getTemplates(context)
     pageContext.isComplete = await McpTemplate.isComplete(context)
 
     return this.showView({ h, pageContext })
