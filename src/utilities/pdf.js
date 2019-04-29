c983bc48bd3b78e1a422fd147d913e13d22acefc
rudenoise <rudenoise@gmail.com>
2019-04-29 14:54:13 +0100

    WE-2466: begin writing pdf with pdf make

--- src/utilities/pdf.js
+++ src/utilities/pdf.js
@@ -1,9 +1,58 @@
 'use strict'
 
-const PdfMakePrinter = require('pdfmake')
+const PdfMake = require('pdfmake')
+const fs = require('fs')
 
-const printer = new PdfMakePrinter()
+/*
+const vfs_fonts = require('pdfmake/build/vfs_fonts.js')
+const printer = new PdfMake({
+  Roboto: {
+    normal: Buffer.from(
+      vfs_fonts.pdfMake.vfs['Roboto-Regular.ttf'],
+      'base64'
+    )
+  }
+})
+*/
 
-console.log(printer)
+const printer = new PdfMake({
+  timesRoman: {
+    normal: 'Times-Roman',
+    bold: 'Times-Bold',
+    italics: 'Times-Italic',
+    bolditalics: 'Times-BoldItalic'
+  }
+})
+
+const docDefinition = {
+  defaultStyle: { font: 'timesRoman' },
+  content: [
+    'First paragraph',
+    'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+  ]
+}
+
+const pdfDoc = printer.createPdfKitDocument(docDefinition)
+
+/*
+let chunks = []
+
+let result
+
+pdfDoc.on('readable', function () {
+  let chunk
+  while ((chunk = pdfDoc.read(9007199254740991)) !== null) {
+    chunks.push(chunk)
+  }
+})
+pdfDoc.on('end', function () {
+  result = Buffer.concat(chunks)
+  console.log(result)
+})
+
+// pdfDoc.pipe(process.stdout)
+*/
+pdfDoc.pipe(fs.createWriteStream('myFile.pdf'))
+pdfDoc.end()
 
 module.exports = printer
