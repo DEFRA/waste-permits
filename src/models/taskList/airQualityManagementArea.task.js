a45e5be471dc6d5da47e2333ef831e8e2803caea
ben <ben@surgison.net>
2019-05-10 15:51:12 +0100

    WE-2492 Application can be submitted with missing information from the task list

--- src/models/taskList/airQualityManagementArea.task.js
+++ src/models/taskList/airQualityManagementArea.task.js
@@ -1,20 +1,5 @@
 'use strict'
 
 const BaseTask = require('./base.task')
-const AirQualityManagementAreaModel = require('../airQualityManagementArea.model')
 
-module.exports = class AirQualityManagementArea extends BaseTask {
-  static async checkComplete (context) {
-    const aqma = await AirQualityManagementAreaModel.get(context)
-
-    return Boolean(
-      (
-        aqma.isInAqma === false || (
-          aqma.isInAqma &&
-          aqma.name &&
-          aqma.nitrogenDioxideLevel &&
-          aqma.localAuthorityName
-        ))
-    )
-  }
-}
+module.exports = class AirQualityManagementArea extends BaseTask {}
--- src/models/taskList/bestAvailableTechniquesAssessment.task.js
+++ src/models/taskList/bestAvailableTechniquesAssessment.task.js
@@ -1,12 +1,5 @@
 'use strict'
 
-const Constants = require('../../constants')
 const BaseTask = require('./base.task')
-const Annotation = require('../../persistence/entities/annotation.entity')
 
-module.exports = class BestAvailableTechniquesAssessment extends BaseTask {
-  static async checkComplete (context) {
-    const evidence = await Annotation.listByApplicationIdAndSubject(context, Constants.UploadSubject.BEST_AVAILABLE_TECHNIQUES_ASSESSMENT)
-    return Boolean(evidence.length)
-  }
-}
+module.exports = class BestAvailableTechniquesAssessment extends BaseTask {}
