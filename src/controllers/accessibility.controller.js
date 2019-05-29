73f2ed32dd1d3b22744f3129548cf7e478787a40
ben <ben@surgison.net>
2019-05-29 09:18:19 +0100

    Add accessibility statement

---
+++ src/controllers/accessibility.controller.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const BaseController = require('./base.controller')
+const { COOKIES } = require('../routes')
+
+module.exports = class AccessibilityController extends BaseController {
+  async doGet (request, h, errors) {
+    const pageContext = this.createPageContext(h, errors)
+    pageContext.cookieRoute = COOKIES.path
+    return this.showView({ h, pageContext })
+  }
+}
