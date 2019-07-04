82a2c763384d7404eae57c3857081ed93194a15f
Stuart Adair <stu@silverka.co.uk>
2019-07-04 11:07:06 +0100

    mcp bespoke tasklist correctly returns false if task not in tasklist

--- src/models/taskList/mcpBespoke.taskList.js
+++ src/models/taskList/mcpBespoke.taskList.js
@@ -12,12 +12,26 @@ module.exports = class mcpBespokeTaskList extends BaseTaskList {
     const { taskDeterminants } = context
     const currentTaskDeterminants = task.determinants
 
+    // get list of tasks and flatten it
+    // reduce is used since flat method isn't supported in Node 10
+    const tasksFromTasklist = this.taskListTemplate.map(item => {
+      return item.tasks.map(task => {
+        return task.id
+      })
+    }).reduce((prev, curr) => prev.concat(curr))
+
+    // if current task isn't in list of tasks then return false straight away
+    // as no further checks are necessary
+    if (!tasksFromTasklist.includes(task.id)) {
+      return false
+    }
+
     // this task is available if there are no determinants listed
     // or if none of the listed determinants are false in taskDeterminants
     const available = !currentTaskDeterminants ||
-     !currentTaskDeterminants.some(item => {
-       return taskDeterminants[item] === false
-     })
+      !currentTaskDeterminants.some(item => {
+        return taskDeterminants[item] === false
+      })
 
     return task.required || (task.route && available)
   }
