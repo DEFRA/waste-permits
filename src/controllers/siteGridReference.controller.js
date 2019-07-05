b1769501c3000afb0a08eb3216c48e84cca69c16
Stuart Adair <stu@silverka.co.uk>
2019-07-05 15:41:59 +0100

    Text of site grid ref page now determined based on whether standard rules

--- src/controllers/siteGridReference.controller.js
+++ src/controllers/siteGridReference.controller.js
@@ -4,12 +4,17 @@ const Routes = require('../routes')
 const BaseController = require('./base.controller')
 const SiteNameAndLocation = require('../models/taskList/siteNameAndLocation.task')
 const RecoveryService = require('../services/recovery.service')
+const Constants = require('../constants')
 
 module.exports = class SiteGridReferenceController extends BaseController {
   async doGet (request, h, errors) {
     const pageContext = this.createPageContext(h, errors)
+
     // Load entity context within the request object
-    await RecoveryService.createApplicationContext(h)
+    const { isStandardRule } = await RecoveryService.createApplicationContext(h)
+
+    // Determine heading and text on page based on whether application is standard rules
+    await customGridRefText(pageContext, isStandardRule)
 
     if (request.payload) {
       // If we have Site details in the payload then display them in the form
@@ -19,6 +24,7 @@ module.exports = class SiteGridReferenceController extends BaseController {
         'site-grid-reference': await SiteNameAndLocation.getGridReference(request)
       }
     }
+
     return this.showView({ h, pageContext })
   }
 
@@ -30,4 +36,14 @@ module.exports = class SiteGridReferenceController extends BaseController {
 
     return this.redirect({ h, route: Routes.POSTCODE_SITE })
   }
+}
+
+async function customGridRefText (pageContext, isStandardRule) {
+  const pageGridRefText = isStandardRule
+    ? 'centre of the site'
+    : 'site’s main emissions point'
+
+  pageContext.pageGridRefText = pageGridRefText
+  pageContext.pageHeading = `What is the grid reference for the ${pageGridRefText}?`
+  pageContext.pageTitle = Constants.buildPageTitle(pageContext.pageHeading)
 }
