0a2931b2153b41a8b0762a37cce377fdb8b0a1f1
Stuart Adair <stu@silverka.co.uk>
2019-08-02 14:12:18 +0100

    Amended text

--- src/routes.js
+++ src/routes.js
@@ -918,8 +918,8 @@ const Routes = {
   FACILITY_APPLY_OFFLINE: {
     path: '/facility-type/apply-offline',
     view: 'bespokeApplyOffline',
-    pageHeading: 'Apply for a bespoke permit for an installation, landfill, mine, waste or water discharge',
-    pageDescription: 'You cannot apply online yet for that type of facility. We hope to add them soon.',
+    pageHeading: 'Apply for a bespoke permit for an installation, landfill, mine or water discharge',
+    pageDescription: 'You cannot apply for that type of facility online yet. We hope to add them soon.',
     controller: 'bespokeApplyOffline',
     nextRoute: 'FACILITY_TYPE',
     types: 'GET',
