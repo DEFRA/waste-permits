cafa21daa91e8773cef3b17fd01fbe40eea3e2ef
David Thomas <david.thomas@environment-agency.gov.uk>
2019-09-02 13:51:33 +0100

    Fix the bespoke check your answers so it works when bypassing the completeness check (#711)
    Fix for https://eaflood.atlassian.net/browse/WE-3125

--- src/models/taskList/bespoke.taskList.js
+++ src/models/taskList/bespoke.taskList.js
@@ -1,4 +1,3 @@
-
 const { bespoke } = require('../../tasks')
 const BaseTaskList = require('./base.taskList')
 const Task = require('../../persistence/entities/task.entity')
@@ -8,16 +7,10 @@ module.exports = class BespokeTaskList extends BaseTaskList {
     return bespoke
   }
 
-  async getSections () {
-    const { context } = this
-    // preload available tasks for use in isAvailable
-    await Task.getAvailableTasks(context)
-    return super.getSections()
-  }
-
   async isAvailable (task = {}) {
     const { context } = this
-    const availableTasks = context.availableTasks.map(({ shortName }) => shortName)
-    return task.required || (task.route && availableTasks.includes(task.shortName))
+    const availableTasks = await Task.getAvailableTasks(context)
+    const taskNames = availableTasks.map(({ shortName }) => shortName)
+    return task.required || (task.route && taskNames.includes(task.shortName))
   }
 }
