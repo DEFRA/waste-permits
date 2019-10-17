7d6c32440a3aed42dff80c450d8da6c84a1b05c9
rudenoise <rudenoise@gmail.com>
2019-10-17 16:04:48 +0100

    WE-2964: add tests

---
+++ test/routes/technicalStandards.route.test.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const Lab = require('@hapi/lab')
+const lab = exports.lab = Lab.script()
+const sinon = require('sinon')
+
+const TechnicalStandards = require('../../src/models/taskList/technicalStandards.task')
+
+const GeneralTestHelper = require('./generalTestHelper.test')
+const UploadTestHelper = require('./uploadHelper')
+
+let fakeAnnotationId = 'ANNOTATION_ID'
+
+const routePath = '/technical-standards/upload'
+const paths = {
+  routePath,
+  uploadPath: `${routePath}/upload`,
+  removePath: `${routePath}/remove/${fakeAnnotationId}`,
+  nextRoutePath: '/task-list'
+}
+
+const helper = new UploadTestHelper(lab, paths)
+
+let sandbox
+
+lab.beforeEach(() => {
+  // Stub methods
+  sandbox = sinon.createSandbox()
+
+  sandbox.stub(TechnicalStandards, 'updateCompleteness').value(() => Promise.resolve({}))
+
+  helper.setStubs(sandbox)
+})
+
+lab.afterEach(() => {
+  // Restore stubbed methods
+  sandbox.restore()
+})
+
+lab.experiment.only('Upload Technical Standards tests:', () => {
+  const { routePath, nextRoutePath } = paths
+  new GeneralTestHelper({ lab, routePath, nextRoutePath }).test({
+    excludeCookiePostTests: true
+  })
+
+  lab.experiment(`GET ${routePath}`, () => {
+    const options = {
+      descriptionId: 'list-technical-standards-description',
+      pageHeading: 'List the technical standards you use',
+      submitButton: 'Continue',
+      fileTypes: ['PDF', 'DOC', 'DOCX', 'ODT']
+    }
+
+    // Perform general get tests
+    helper.getSuccess(options)
+    helper.getFailure()
+  })
+  const { uploadPath, removePath } = paths
+  lab.experiment(`GET ${removePath}`, () => {
+    // Perform general remove tests
+    helper.removeSuccess()
+  })
+
+  lab.experiment(`POST ${uploadPath}`, () => {
+    // Perform general upload tests
+    helper.uploadSuccess('application/msword', 'test.docx')
+    helper.uploadInvalid({ fileTypes: ['PDF', 'DOC', 'DOCX', 'ODT'] }, 'application/msword', 'test.docx')
+    helper.uploadFailure('application/msword', 'test.docx')
+  })
+
+  lab.experiment(`POST ${routePath}`, () => {
+    // Perform general post tests
+    helper.postSuccess({ payload: { 'upload-non-technical-summary': 'upload-non-technical-summary' } })
+  })
+})
