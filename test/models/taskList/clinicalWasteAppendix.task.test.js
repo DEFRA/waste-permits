e45df3ca9e11528aa2df840a934b28ed5f1ec709
Stuart Adair <stu@silverka.co.uk>
2019-11-08 14:37:07 +0000

    Fixed issue and updated tests

--- test/models/taskList/clinicalWasteAppendix.task.test.js
+++ test/models/taskList/clinicalWasteAppendix.task.test.js
@@ -4,19 +4,25 @@ const Lab = require('@hapi/lab')
 const lab = exports.lab = Lab.script()
 const Code = require('@hapi/code')
 const sinon = require('sinon')
+const Mocks = require('../../helpers/mocks')
 
 const Annotation = require('../../../src/persistence/entities/annotation.entity')
 const ClinicalWasteAppendix = require('../../../src/models/taskList/clinicalWasteAppendix.task')
+const StoreTreatModel = require('../../../src/models/clinicalWasteDocuments/storeTreat.model')
 
 let sandbox
+let mocks
 let listAnnotationsStub
 
 lab.beforeEach(() => {
+  mocks = new Mocks()
+
   // Create a sinon sandbox
   sandbox = sinon.createSandbox()
 
   // Stub the asynchronous model methods
   listAnnotationsStub = sandbox.stub(Annotation, 'listByApplicationIdAndSubject')
+  sandbox.stub(StoreTreatModel, 'get').callsFake(async () => mocks.storeTreat)
   listAnnotationsStub.resolves([{}])
 })
 
@@ -26,25 +32,42 @@ lab.afterEach(() => {
 })
 
 lab.experiment('Task List: ClinicalWasteAppendix Model tests:', () => {
-  lab.test('checkComplete() method correctly returns FALSE when no justification provided', async () => {
+  lab.test('checkComplete() method correctly returns FALSE when justifcation question is not answered', async () => {
+    mocks.storeTreat.storeTreat = undefined
+    const result = await ClinicalWasteAppendix.checkComplete(mocks.context)
+    Code.expect(result).to.equal(false)
+  })
+
+  lab.test('checkComplete() method correctly returns FALSE when justification needed and not provided', async () => {
+    mocks.storeTreat.storeTreat = true
     listAnnotationsStub.withArgs({}, 'clinical waste non-standard justification').resolves([])
     const result = await ClinicalWasteAppendix.checkComplete({})
     Code.expect(result).to.equal(false)
   })
 
   lab.test('checkComplete() method correctly returns FALSE when no treatment summary provided', async () => {
+    mocks.storeTreat.storeTreat = false
     listAnnotationsStub.withArgs({}, 'clinical waste treatment summary').resolves([])
     const result = await ClinicalWasteAppendix.checkComplete({})
     Code.expect(result).to.equal(false)
   })
 
   lab.test('checkComplete() method correctly returns FALSE when no layout plan provided', async () => {
+    mocks.storeTreat.storeTreat = false
     listAnnotationsStub.withArgs({}, 'clinical waste layout plans and process flows').resolves([])
     const result = await ClinicalWasteAppendix.checkComplete({})
     Code.expect(result).to.equal(false)
   })
 
+  lab.test('checkComplete() method correctly returns TRUE when all uploads provided, except justification when not required', async () => {
+    mocks.storeTreat.storeTreat = false
+    listAnnotationsStub.withArgs({}, 'clinical waste non-standard justification').resolves([])
+    const result = await ClinicalWasteAppendix.checkComplete({})
+    Code.expect(result).to.equal(true)
+  })
+
   lab.test('checkComplete() method correctly returns TRUE when all uploads provided', async () => {
+    mocks.storeTreat.storeTreat = true
     const result = await ClinicalWasteAppendix.checkComplete({})
     Code.expect(result).to.equal(true)
   })
