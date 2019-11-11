d160f8665ada465e1103d23d6ed22050ba4281c9
Stuart Adair <stu@silverka.co.uk>
2019-11-11 10:16:58 +0000

    Clinical and hazardous waste only added to tasklist when waste types selected

--- src/models/taskList/bespoke.taskList.js
+++ src/models/taskList/bespoke.taskList.js
@@ -1,6 +1,14 @@
-const { bespoke, tasks: { WASTE_WEIGHTS: { shortName: wasteWeightsShortName } } } = require('../../tasks')
+const { bespoke,
+  tasks: {
+    WASTE_WEIGHTS: { shortName: wasteWeightsShortName },
+    CLINICAL_WASTE_APPENDIX: { shortName: clinicalWasteShortName },
+    MANAGE_HAZARDOUS_WASTE: { shortName: hazardousWasteShortName }
+  }
+} = require('../../tasks')
+
 const BaseTaskList = require('./base.taskList')
 const Task = require('../../persistence/entities/task.entity')
+const DataStore = require('../../models/dataStore.model')
 
 module.exports = class BespokeTaskList extends BaseTaskList {
   get taskListTemplate () {
@@ -10,8 +18,20 @@ module.exports = class BespokeTaskList extends BaseTaskList {
   async isAvailable (task = {}) {
     const { context } = this
     const availableTasks = await Task.getAvailableTasks(context)
-    const taskNames = availableTasks.map(({ shortName }) => shortName)
+    const { data: { acceptsClinicalWaste, acceptsHazardousWaste } } = await DataStore.get(context)
+
+    // Remove clinical and hazardous waste from the tasklist when mapping
+    // They are only shown if the applicant specifically states they accept those waste types
+    const taskNames = availableTasks
+      .map(({ shortName }) => shortName)
+      .filter((shortName) => shortName !== clinicalWasteShortName && shortName !== hazardousWasteShortName)
+
+    // Add clinical and hazardous waste to the tasklist if applicant has stated they accept them
+    if (acceptsClinicalWaste) { taskNames.push(clinicalWasteShortName) }
+    if (acceptsHazardousWaste) { taskNames.push(hazardousWasteShortName) }
+
     taskNames.push(wasteWeightsShortName)
+
     return task.required || (task.route && taskNames.includes(task.shortName))
   }
 }
