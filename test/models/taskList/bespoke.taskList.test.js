25a6493171d28f29ece4dee73672ffadea2f69f4
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2019-11-11 15:16:36 +0000

    Clinical and hazardous waste only added to tasklist when waste types selected

---
+++ test/models/taskList/bespoke.taskList.test.js
@@ -0,0 +1,85 @@
+'use strict'
+
+const Lab = require('@hapi/lab')
+const lab = exports.lab = Lab.script()
+const Code = require('@hapi/code')
+const sinon = require('sinon')
+const Mocks = require('../../helpers/mocks')
+
+const BespokeTaskList = require('../../../src/models/taskList/bespoke.taskList')
+const Task = require('../../../src/persistence/entities/task.entity')
+const DataStore = require('../../../src/models/dataStore.model')
+
+const availableTasks = []
+
+const { tasks: {
+  CLINICAL_WASTE_APPENDIX: { shortName: clinicalWasteShortName },
+  MANAGE_HAZARDOUS_WASTE: { shortName: hazardousWasteShortName }
+} } = require('../../../src/tasks')
+
+const clinicalWasteTask = { shortName: clinicalWasteShortName, route: true }
+const hazardousWasteTask = { shortName: hazardousWasteShortName, route: true }
+
+let context
+let sandbox
+let mocks
+let TestTaskList
+
+lab.beforeEach(() => {
+  mocks = new Mocks()
+
+  context = mocks.context
+
+  TestTaskList = new BespokeTaskList(context)
+
+  // Create a sinon sandbox to stub methods
+  sandbox = sinon.createSandbox()
+
+  // Stub methods
+  sandbox.stub(Task, 'getAvailableTasks').value(async () => availableTasks)
+})
+
+lab.afterEach(() => {
+  // Restore the sandbox to make sure the stubs are removed correctly
+  sandbox.restore()
+})
+
+lab.experiment('Bespoke Task List Model tests:', () => {
+  lab.experiment('Clinical Waste', () => {
+    lab.test('isAvailable() returns TRUE when the activity is not selected and the waste type is accepted', async () => {
+      sandbox.stub(DataStore, 'get').value(() => { return { data: { acceptsClinicalWaste: true } } })
+
+      const available = await TestTaskList.isAvailable(clinicalWasteTask)
+
+      Code.expect(available).to.be.true()
+    })
+
+    lab.test('isAvailable() returns TRUE when the activity is not selected and the waste type is accepted', async () => {
+      sandbox.stub(DataStore, 'get').value(() => { return { data: { acceptsHazardousWaste: true } } })
+
+      const available = await TestTaskList.isAvailable(hazardousWasteTask)
+
+      Code.expect(available).to.be.true()
+    })
+  })
+
+  lab.experiment('Hazardous Waste', () => {
+    lab.test('isAvailable() returns FALSE when the activity is selected and the waste type is not accepted', async () => {
+      sandbox.stub(DataStore, 'get').value(() => { return { data: { acceptsClinicalWaste: false } } })
+      availableTasks.push(clinicalWasteTask)
+
+      const available = await TestTaskList.isAvailable(clinicalWasteTask)
+
+      Code.expect(available).to.be.false()
+    })
+
+    lab.test('isAvailable() returns FALSE when the activity is selected and the waste type is not accepted', async () => {
+      sandbox.stub(DataStore, 'get').value(() => { return { data: { acceptsHazardousWaste: false } } })
+      availableTasks.push(hazardousWasteTask)
+
+      const available = await TestTaskList.isAvailable(hazardousWasteTask)
+
+      Code.expect(available).to.be.false()
+    })
+  })
+})
