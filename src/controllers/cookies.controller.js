16196e311e207c5160cda3bb1e15f8b123c39b6c
Ben Sagar <benjamin.sagar@environment-agency.gov.uk>
2021-05-11 13:08:23 +0100

    Remove tracking cookies
    To ensure compliance with regulations, tracking cookies are removed and the cookies page is updated to reflect this.

--- src/controllers/cookies.controller.js
+++ src/controllers/cookies.controller.js
@@ -22,35 +22,6 @@ const sections = [
     ]
   },
   {
-    heading: 'Cookies for measuring usage (Google Analytics)',
-    intro: [
-      `We use Google Analytics software to collect information about how you use the government’s digital services. We do this to help make sure the service is meeting the needs of its users and to help us make improvements.`,
-      `Google Analytics stores information about the pages you visit in this service. We do not collect or store your personal information (for example your name or address) so this information cannot be used to identify who you are.`,
-      `We do not allow Google to use or share our analytics data.`
-    ],
-    id: 'analytics-cookie',
-    cookies: [
-      {
-        id: 'ga-cookie',
-        name: '_ga',
-        purpose: 'This helps us count how many people visit the service by tracking if you have visited before',
-        expires: '2 years'
-      },
-      {
-        id: 'gid-cookie',
-        name: '_gid',
-        purpose: 'This helps us count how many people visit the service by tracking if you have visited before',
-        expires: '24 hours'
-      },
-      {
-        id: 'gat-cookie',
-        name: '_gat',
-        purpose: 'This helps us count how many people visit the service by tracking if you have visited before',
-        expires: '10 minutes'
-      }
-    ]
-  },
-  {
     heading: 'Introductory message cookie',
     intro: [`You may see a pop-up welcome message when you first visit this service. We will store a cookie so that your computer knows you have seen it and knows not to show it again.`],
     id: 'introductory-message-cookie',
